0b8ef8785b175a42f7e77aef6fda64dea668ed29
Bryce <bryce@Bryces-MacBook-Air.local>
2014-06-13 13:33:08 -0700

    Remove code for removing spurious ASSERT warnings in debug log

--- OpenStreetMap/OSMiOS/OSMiOS/main.m
+++ OpenStreetMap/OSMiOS/OSMiOS/main.m
@@ -10,8 +10,29 @@
 
 #import "AppDelegate.h"
 
+
+#if 0 // ASSERT bug
+typedef int (*PYStdWriter)(void *, const char *, int);
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
+        return 0;
+    }
+    return _oldStdWrite(inFD, buffer, size);
+}
+#endif
+
+
+
 int main(int argc, char *argv[])
 {
+#if 0
+	_oldStdWrite = stderr->_write;
+    stderr->_write = __pyStderrWrite;
+#endif
+	
 	@autoreleasepool {
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
 	}
