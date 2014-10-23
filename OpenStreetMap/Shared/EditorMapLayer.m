a2a8461f59b6abb1b68446c94c5275d25c525833
Bryce <bryce@Bryces-MacBook-Air.local>
2014-10-22 20:26:34 -0700

    Use GeekBench scores to set number of objects.

--- OpenStreetMap/Shared/EditorMapLayer.m
+++ OpenStreetMap/Shared/EditorMapLayer.m
@@ -920,49 +920,63 @@ static NSInteger ClipLineToRect( OSMPoint p1, OSMPoint p2, OSMRect rect, OSMPoin
 
 #pragma mark Drawing
 
--(BOOL)hasFastGraphics
+-(double)geekbenchScore
 {
-	static BOOL isFast = YES;
+	static double score = 0;
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
 		struct utsname systemInfo = { 0 };
 		uname(&systemInfo);
 		NSString * name = [[NSString alloc] initWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 		NSDictionary * dict = @{
-							  @"i386"      :@"Simulator",
-							  @"iPod1,1"   :@"iPod Touch",      // (Original)
-							  @"iPod2,1"   :@"iPod Touch",      // (Second Generation)
-							  @"iPod3,1"   :@"iPod Touch",      // (Third Generation)
-							  @"iPod4,1"   :@"iPod Touch",      // (Fourth Generation)
-							  @"iPhone1,1" :@"iPhone",          // (Original)
-							  @"iPhone1,2" :@"iPhone",          // (3G)
-							  @"iPhone2,1" :@"iPhone",          // (3GS)
-							  @"iPad1,1"   :@"iPad",            // (Original)
-							  @"iPad2,1"   :@"iPad 2",          //
-							  @"iPad3,1"   :@"iPad",            // (3rd Generation)
-							  @"iPhone3,1" :@"iPhone 4",        //
-							  @"iPhone4,1" :@"iPhone 4S",       //
-//							  @"iPhone5,1" :@"iPhone 5",        // (model A1428, AT&T/Canada)
-//							  @"iPhone5,2" :@"iPhone 5",        // (model A1429, everything else)
-							  @"iPad3,3"   :@"iPad",            // (3rd Generation)
-//							  @"iPad3,4"   :@"iPad",            // (4th Generation)
-							  @"iPad2,5"   :@"iPad Mini",       // (Original)
-//							  @"iPhone5,3" :@"iPhone 5c",       // (model A1456, A1532 | GSM)
-//							  @"iPhone5,4" :@"iPhone 5c",       // (model A1507, A1516, A1526 (China), A1529 | Global)
-//							  @"iPhone6,1" :@"iPhone 5s",       // (model A1433, A1533 | GSM)
-//							  @"iPhone6,2" :@"iPhone 5s",       // (model A1457, A1518, A1528 (China), A1530 | Global)
-//							  @"iPad4,1"   :@"iPad Air",        // 5th Generation iPad (iPad Air) - Wifi
-//							  @"iPad4,2"   :@"iPad Air",        // 5th Generation iPad (iPad Air) - Cellular
-//							  @"iPad4,4"   :@"iPad Mini",       // (2nd Generation iPad Mini - Wifi)
-//							  @"iPad4,5"   :@"iPad Mini"        // (2nd Generation iPad Mini - Cellular)
-				};
+								@"x86_64"    :	@4000,				// Simulator
+								@"i386"      :	@4000,				// Simulator
+
+								@"iPad5,4"	 :	@"",				// iPad Air 2
+								@"iPad4,5"   :	@2493,				// iPad Mini (2nd Generation iPad Mini - Cellular)
+								@"iPad4,4"   :	@2493,				// iPad Mini (2nd Generation iPad Mini - Wifi)
+								@"iPad4,2"   :	@2664,				// iPad Air 5th Generation iPad (iPad Air) - Cellular
+								@"iPad4,1"   :	@2664,				// iPad Air 5th Generation iPad (iPad Air) - Wifi
+								@"iPad3,6"   :	@1402,				// iPad 4 (4th Generation)
+								@"iPad3,5"   :	@1402,				// iPad 4 (4th Generation)
+								@"iPad3,4"   :	@1402,				// iPad 4 (4th Generation)
+								@"iPad3,3"   :	@492,				// iPad 3 (3rd Generation)
+								@"iPad3,2"   :	@492,				// iPad 3 (3rd Generation)
+								@"iPad3,1"   :	@492,				// iPad 3 (3rd Generation)
+								@"iPad2,7"   :	@490,				// iPad Mini (Original)
+								@"iPad2,6"   :	@490,				// iPad Mini (Original)
+								@"iPad2,5"   :	@490,				// iPad Mini (Original)
+								@"iPad2,4"   :	@492,				// iPad 2
+								@"iPad2,3"   :	@492,				// iPad 2
+								@"iPad2,2"   :	@492,				// iPad 2
+								@"iPad2,1"   :	@492,				// iPad 2
+
+								@"iPhone7,2" :	@2855,				// iPhone 6+
+								@"iPhone7,1" :	@2879,				// iPhone 6
+								@"iPhone6,2" :	@2523,				// iPhone 5s (model A1457, A1518, A1528 (China), A1530 | Global)
+								@"iPhone6,1" :	@2523,				// iPhone 5s model A1433, A1533 | GSM)
+								@"iPhone5,4" :	@1240,				// iPhone 5c (model A1507, A1516, A1526 (China), A1529 | Global)
+								@"iPhone5,3" :	@1240,				// iPhone 5c (model A1456, A1532 | GSM)
+								@"iPhone5,2" :	@1274,				// iPhone 5 (model A1429, everything else)
+								@"iPhone5,1" :	@1274,				// iPhone 5 (model A1428, AT&T/Canada)
+								@"iPhone4,1" :	@405,				// iPhone 4S
+								@"iPhone3,1" :	@206,				// iPhone 4
+								@"iPhone2,1" :	@150,				// iPhone 3GS
+
+								@"iPod4,1"   :	@410,				// iPod Touch (Fifth Generation)
+								@"iPod4,1"   :	@209,				// iPod Touch (Fourth Generation)
+							};
 		NSString * value = [dict objectForKey:name];
-		if ( value.length > 0 ) {
-			isFast = NO;
+		if ( [value isKindOfClass:[NSNumber class]] ) {
+			score = value.doubleValue;
+		}
+		if ( score == 0 ) {
+			score = 2500;
 		}
 	});
-	return isFast;
+	return score;
 }
+
 
 
 -(BOOL)enableMapCss
@@ -1653,7 +1667,8 @@ enum {
 
 -(NSMutableArray *)getShapeLayersForHighlights
 {
-	NSInteger			nameLimit	= [self hasFastGraphics] ? 10 : 5;
+	double				geekScore	= [self geekbenchScore];
+	NSInteger			nameLimit	= 5 + (geekScore - 500) / 200;	// 500 -> 5, 2500 -> 10
 	NSMutableSet	*	nameSet		= [NSMutableSet new];
 	NSMutableArray	*	layers		= [NSMutableArray new];
 
@@ -2633,7 +2648,8 @@ static BOOL VisibleSizeLessStrict( OsmBaseObject * obj1, OsmBaseObject * obj2 )
 - (NSMutableArray *)getObjectsToDisplay
 {
 #if TARGET_OS_IPHONE
-	NSInteger objectLimit = [self hasFastGraphics] ? 100 : 50;
+	double geekScore = [self geekbenchScore];
+	NSInteger objectLimit = 50 + (geekScore - 500) / 40;	// 500 -> 50, 2500 -> 100;
 #else
 	NSInteger objectLimit = 500;
 #endif
