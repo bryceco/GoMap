1ba90d4c343899268a8864918a0603a5fb1afce7
wtimme <wtimme@users.noreply.github.com>
2019-04-02 21:21:36 +0200

    Add empty `MeasureDirectionViewModel`

---
+++ src/iOS/Direction/MeasureDirectionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MeasureDirectionViewModel.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 4/2/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import Foundation
+
+class MeasureDirectionViewModel: NSObject {
+
+}
