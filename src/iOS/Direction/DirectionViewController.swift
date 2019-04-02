e4de1d6d3059e7a5abed11e60d5c3f2129a94f27
wtimme <wtimme@users.noreply.github.com>
2019-04-02 20:47:55 +0200

    Add empty `DirectionViewController`

---
+++ src/iOS/Direction/DirectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DirectionViewController.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 4/2/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import UIKit
+
+@objc class DirectionViewController: UIViewController {
+    
+    @objc init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
