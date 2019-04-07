8fcaac4cd3accc896a3b350e65617f8cb7d89851
wtimme <wtimme@users.noreply.github.com>
2019-04-07 14:19:22 +0200

    Move presentation of view controller for measuring height to dedicated method
    This improves the readability of the switch/case statement.

--- src/Shared/MapView.m
+++ src/Shared/MapView.m
@@ -2350,11 +2350,7 @@ NSString * ActionTitle( EDIT_ACTION action, BOOL abbrev )
 			}
 			break;
 		case ACTION_HEIGHT:
-			if ( self.gpsState != GPS_STATE_NONE ) {
-				[self.viewController performSegueWithIdentifier:@"CalculateHeightSegue" sender:nil];
-			} else {
-				error = NSLocalizedString(@"This action requires GPS to be turned on",nil);
-			}
+            [self presentViewControllerForMeasuringHeight];
 			break;
 		case ACTION_EDITTAGS:
 			[self presentTagEditor:nil];
@@ -3671,6 +3667,16 @@ static NSString * const DisplayLinkPanning	= @"Panning";
 			_confirmDrag = (_editorLayer.selectedPrimary.modifyCount == 0);
 		}
 	}
+}
+
+- (void)presentViewControllerForMeasuringHeight {
+    if ( self.gpsState != GPS_STATE_NONE ) {
+        [self.viewController performSegueWithIdentifier:@"CalculateHeightSegue" sender:nil];
+    } else {
+        NSString *errorMessage = NSLocalizedString(@"This action requires GPS to be turned on",nil);
+        
+        [self showAlert:errorMessage message:nil];
+    }
 }
 
 @end
