cb886aae3782d2574e4188710b0f76e65f9d1925
wtimme <wtimme@users.noreply.github.com>
2019-04-13 16:48:55 +0200

    #139 Make sure the `placeholder` parameter is preferred (if provided)

--- src/iOS/GoMapTests/CommonTagKeyTestCase.swift
+++ src/iOS/GoMapTests/CommonTagKeyTestCase.swift
@@ -12,6 +12,23 @@ import XCTest
 
 class CommonTagKeyTestCase: XCTestCase {
     
+    func testInitWithPresetsShouldPreferThePlaceholderParameterIfProvided() {
+        let placeholder = "Lorem ipsum"
+        
+        let firstPreset = CommonTagValue(name: "Ja", details: "", tagValue: "yes")
+        let secondPreset = CommonTagValue(name: "Nein", details: "", tagValue: "no")
+        
+        let tagKey = CommonTagKey(name: "Rückenlehne",
+                                  tagKey: "backreset",
+                                  defaultValue: nil,
+                                  placeholder: placeholder,
+                                  keyboard: .default,
+                                  capitalize: .none,
+                                  presets: [firstPreset, secondPreset])
+        
+        XCTAssertEqual(tagKey.require().placeholder, placeholder)
+    }
+    
     func testInitWithPresetsShouldUseTheirNamesForPlaceholder() {
         let firstPresetName = "Ja"
         let firstPreset = CommonTagValue(name: firstPresetName, details: "", tagValue: "yes")
