bd1d34b625029e9ade53c9f9970e1ec3d833108f
wtimme <wtimme@users.noreply.github.com>
2019-05-01 04:20:32 +0200

    Add `RecursiveQuery`
    This object can contain sub-queries and allows for logical
    "and" or "or" combinations.

---
+++ src/iOS/GoMapTests/Overpass/Queries/RecursiveQueryTestCase.swift
@@ -0,0 +1,71 @@
+//
+//  RecursiveQueryTestCase.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 5/1/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import XCTest
+@testable import Go_Map__
+
+class RecursiveQueryTestCase: XCTestCase {
+    
+    func testRecursiveQueryWithLogicalAndAndAllQueriesReturningTrueShouldReturnTrue() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .and, queries: [firstMatchMock, secondMatchMock])
+        
+        firstMatchMock.doesMatch = true
+        secondMatchMock.doesMatch = true
+        
+        XCTAssertTrue(query.matches(OsmBaseObject()))
+    }
+    
+    func testRecursiveQueryWithLogicalAndAndOneQueryReturningFalseShouldReturnFalse() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .and, queries: [firstMatchMock, secondMatchMock])
+        
+        firstMatchMock.doesMatch = true
+        secondMatchMock.doesMatch = false
+        
+        XCTAssertFalse(query.matches(OsmBaseObject()))
+    }
+    
+    func testRecursiveQueryWithLogicalOrAndAllQueriesReturningFalseShouldReturnFalse() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .or, queries: [firstMatchMock, secondMatchMock])
+        
+        firstMatchMock.doesMatch = false
+        secondMatchMock.doesMatch = false
+        
+        XCTAssertFalse(query.matches(OsmBaseObject()))
+    }
+    
+    func testRecursiveQueryWithLogicalOrAndOneQueryReturningTrueShouldReturnTrue() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .or, queries: [firstMatchMock, secondMatchMock])
+        
+        firstMatchMock.doesMatch = false
+        secondMatchMock.doesMatch = true
+        
+        XCTAssertTrue(query.matches(OsmBaseObject()))
+    }
+    
+    func testRecursiveQueryWithLogicalOrShouldStopAskingForMatchesWhenAMatchWasFound() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .or, queries: [firstMatchMock, secondMatchMock])
+        
+        firstMatchMock.doesMatch = true
+        
+        XCTAssertTrue(query.matches(OsmBaseObject()))
+        
+        XCTAssertNil(secondMatchMock.object,
+                     "Since the first query yielded a positive result, the second one should not have been evaluated.")
+    }
+
+}
