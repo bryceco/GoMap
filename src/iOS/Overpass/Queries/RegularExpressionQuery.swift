ffc176e1ddbb5443121437aeceda286660b0dee2
wtimme <wtimme@users.noreply.github.com>
2019-05-05 18:10:29 +0200

    Add `RegularExpressionQuery` that allows to query by regex

---
+++ src/iOS/Overpass/Queries/RegularExpressionQuery.swift
@@ -0,0 +1,24 @@
+//
+//  RegularExpressionQuery.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 5/5/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+struct RegularExpressionQuery {
+    let key: String
+    let value: String
+}
+
+extension RegularExpressionQuery: BaseObjectMatching {
+    
+    func matches(_ object: OsmBaseObject) -> Bool {
+        guard let tags = object.tags as? [String: String] else { return false }
+        
+        return tags.first { tagKey, tagValue in
+            return tagKey.range(of: key, options: .regularExpression) != nil && tagValue.range(of: value, options: .regularExpression) != nil
+        } != nil
+    }
+    
+}
