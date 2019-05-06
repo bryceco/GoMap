5491b93015370e9b3a736e347ea74a39e38a802f
wtimme <wtimme@users.noreply.github.com>
2019-05-07 00:01:54 +0200

    Add test case for `QueryFormViewModel`

---
+++ src/iOS/GoMapTests/Overpass/QueryFormViewModelTestCase.swift
@@ -0,0 +1,29 @@
+//
+//  QueryFormViewModelTestCase.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 5/7/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import XCTest
+
+@testable import Go_Map__
+
+class QueryFormViewModelTestCase: XCTestCase {
+    
+    var viewModel: QueryFormViewModel!
+
+    override func setUp() {
+        super.setUp()
+        
+        viewModel = QueryFormViewModel()
+    }
+
+    override func tearDown() {
+        viewModel = nil
+        
+        super.tearDown()
+    }
+
+}
