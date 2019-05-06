744fb9371ab3e0ade4857861d22db5903c15339a
wtimme <wtimme@users.noreply.github.com>
2019-05-06 21:40:46 +0200

    Add Swift extension for `DisplayViewController`
    This file will contain the Swift code for it.

---
+++ src/iOS/Extensions/DisplayViewController.swift
@@ -0,0 +1,11 @@
+//
+//  DisplayViewController.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 5/6/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+extension DisplayViewController {
+    
+}
