8d406eff1820fca949793a49b89886b7196b6a35
wtimme <wtimme@users.noreply.github.com>
2019-05-06 20:35:03 +0200

    Create `QueryFormViewController`

---
+++ src/iOS/Overpass/QueryForm/QueryFormViewController.swift
@@ -0,0 +1,13 @@
+//
+//  QueryFormViewController.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 5/6/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import UIKit
+
+class QueryFormViewController: UIViewController {
+
+}
