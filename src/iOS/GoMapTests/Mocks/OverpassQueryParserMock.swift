f0b26460dcf35f8377ce655a82b90b90b0aec901
wtimme <wtimme@users.noreply.github.com>
2019-05-07 00:24:43 +0200

    Add mock for `OverpassQueryParsing`
    This will allow us to mock the behaviour of the parser
    in unit tests.

---
+++ src/iOS/GoMapTests/Mocks/OverpassQueryParserMock.swift
@@ -0,0 +1,25 @@
+//
+//  OverpassQueryParserMock.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 5/7/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import Foundation
+@testable import Go_Map__
+
+class OverpassQueryParserMock: NSObject {
+    var query: String?
+    var mockedResult: OverpassQueryParserResult = .error("")
+}
+
+extension OverpassQueryParserMock: OverpassQueryParsing {
+    
+    func parse(_ query: String) -> OverpassQueryParserResult {
+        self.query = query
+        
+        return mockedResult
+    }
+    
+}
