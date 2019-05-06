f4968fbb49a47c03abfe000b13d806a37aecd815
wtimme <wtimme@users.noreply.github.com>
2019-05-06 23:59:29 +0200

    Add `QueryFormViewModel`

---
+++ src/iOS/Overpass/QueryForm/QueryFormViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  QueryFormViewModel.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 5/6/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import Foundation
+
+class QueryFormViewModel: NSObject {
+
+}
