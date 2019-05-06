512f29471865e4270edcb1ee6a62daa6769e177f
wtimme <wtimme@users.noreply.github.com>
2019-05-06 21:44:05 +0200

    Add UI test for the `OverpassQueryFormViewController`

---
+++ src/iOS/GoMapUITests/OverpassQueryFormUITestCase.swift
@@ -0,0 +1,46 @@
+//
+//  OverpassQueryFormUITestCase.swift
+//  GoMapUITests
+//
+//  Created by Wolfgang Timme on 5/6/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+import XCTest
+
+class OverpassQueryFormUITestCase: XCTestCase {
+
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        app = XCUIApplication()
+        app.launch()
+    }
+    
+    override func tearDown() {
+        app.terminate()
+        app = nil
+    }
+    
+    func testTapOnOverpassQueryMenuItemInDisplayOptionsShouldShowOverpassQueryFormViewController() {
+        goToOverpassQueryViewController()
+    }
+    
+    // MARK: Helper methods
+    
+    private func goToOverpassQueryViewController() {
+        let button = app.buttons["display_options_button"]
+        button.press(forDuration: 1.0)
+        
+        waitForViewController("Display")
+        
+        app.cells["overpass_query"].tap()
+        
+        waitForViewController("Overpass Query")
+    }
+
+}
