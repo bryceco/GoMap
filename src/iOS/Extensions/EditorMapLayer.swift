934b3249d6c9ec41e71382786057b1a9848d6983
wtimme <wtimme@users.noreply.github.com>
2019-05-10 19:27:23 +0200

    Add Swift extension for `EditorMapLayer`

---
+++ src/iOS/Extensions/EditorMapLayer.swift
@@ -0,0 +1,11 @@
+//
+//  EditorMapLayer.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 5/10/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+@objc extension EditorMapLayer {
+    
+}
