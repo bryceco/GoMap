4ec498664c35ae3c7f5c150b96f7c14f23b1c62a
wtimme <wtimme@users.noreply.github.com>
2020-02-02 16:45:59 +0100

    Add test case for `GeoURLParser`

---
+++ src/iOS/GoMapTests/GeoURLParserTestCase.swift
@@ -0,0 +1,56 @@
+//
+//  GeoURLParserTestCase.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 2/2/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import XCTest
+@testable import Go_Map__
+
+class GeoURLParserTestCase: XCTestCase {
+    
+    var parser: GeoURLParser!
+
+    override func setUp() {
+        super.setUp()
+        
+        parser = GeoURLParser()
+    }
+
+    override func tearDown() {
+        parser = nil
+        
+        super.tearDown()
+    }
+
+    func testParseURL_withSchemeThatIsNotGeo_shouldResultInNil() {
+        /// Given
+        let url = URL(string: "https://openstreetmap.org/").require()
+        
+        /// When
+        let result = parser.parseURL(url)
+        
+        /// Then
+        XCTAssertNil(result)
+    }
+
+    func testParseURL_withProperURL_shouldReturnProperResult() {
+        /// Given
+        let latitude: Double = 1
+        let longitude: Double = 2
+        let zoom: Double = 3
+        let url = URL(string: "geo:\(latitude),\(longitude)?z=\(zoom)").require()
+        
+        /// When
+        let result = parser.parseURL(url).require()
+        
+        /// Then
+        XCTAssertEqual(result.latitude, latitude)
+        XCTAssertEqual(result.longitude, longitude)
+        XCTAssertEqual(result.zoom, zoom)
+        XCTAssertEqual(result.viewState, MAPVIEW_NONE)
+    }
+
+}
