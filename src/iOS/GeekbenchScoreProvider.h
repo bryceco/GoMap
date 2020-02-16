31c6c7b3b9f2b124432d0e48b553a66ca595e59e
wtimme <wtimme@users.noreply.github.com>
2020-02-16 11:00:00 +0100

    Add empty class `GeekbenchScoreProvider`
    This will later contain the implementation of the
    Geekscore calculation.

---
+++ src/iOS/GeekbenchScoreProvider.h
@@ -0,0 +1,17 @@
+//
+//  GeekbenchScoreProvider.h
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 2/16/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GeekbenchScoreProvider : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/iOS/GeekbenchScoreProvider.m
@@ -0,0 +1,13 @@
+//
+//  GeekbenchScoreProvider.m
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 2/16/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+#import "GeekbenchScoreProvider.h"
+
+@implementation GeekbenchScoreProvider
+
+@end
