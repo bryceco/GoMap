f2d01c80eb5338a7aa25a65bdf87ae95088aae77
wtimme <wtimme@users.noreply.github.com>
2020-02-28 10:40:22 +0100

    #264 Create method for opening the OSM wiki page for help

--- src/iOS/Extensions/MapViewController.swift
+++ src/iOS/Extensions/MapViewController.swift
@@ -7,7 +7,14 @@
 //
 
 import UIKit
+import SafariServices
 
 extension MapViewController {
-    
+    @IBAction func openHelp() {
+        let urlAsString = "https://wiki.openstreetmap.org/w/index.php?title=Go_Map!!&mobileaction=toggle_view_mobile"
+        guard let url = URL(string: urlAsString) else { return }
+        
+        let safariViewController = SFSafariViewController(url: url)
+        present(safariViewController, animated: true)
+    }
 }
