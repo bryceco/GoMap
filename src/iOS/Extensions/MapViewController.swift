defd38b48cfafb8245cddcac664fa9a05b284654
wtimme <wtimme@users.noreply.github.com>
2020-02-28 10:34:10 +0100

    #264 Add Swift extension on `MapViewController`
    This extension allows for the addition of new code to the
    view controller, while at the same time avoiding to write
    legacy Objective-C code.

---
+++ src/iOS/Extensions/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 2/28/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import UIKit
+
+extension MapViewController {
+    
+}
