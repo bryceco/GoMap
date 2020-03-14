bryceco/GoMap

commit 3e24d761478e1bb1e44df33492c47242b1f04344

Fixes: End of line does not snap to building which is part of a relation #281

src/Shared/EditorMapLayer.m
@@ -2620,8 +2620,13 @@ inline static CGFloat HitTestLineSegment(CLLocationCoordinate2D point, OSMSize m
 }
 
 // distance is in units of the hit test radius (WayHitTestRadius)
-+ (void)osmHitTestEnumerate:(CGPoint)point radius:(CGFloat)radius mapView:(MapView *)mapView objects:(NSArray<OsmBaseObject *> *)objects testNodes:(BOOL)testNodes
-				 ignoreList:(NSArray<OsmBaseObject *> *)ignoreList block:(void(^)(OsmBaseObject * obj,CGFloat dist,NSInteger segment))block
++ (void)osmHitTestEnumerate:(CGPoint)point
+					 radius:(CGFloat)radius
+					mapView:(MapView *)mapView
+					objects:(NSArray<OsmBaseObject *> *)objects
+				  testNodes:(BOOL)testNodes
+				 ignoreList:(NSArray<OsmBaseObject *> *)ignoreList
+					  block:(void(^)(OsmBaseObject * obj,CGFloat dist,NSInteger segment))block
 {
 	CLLocationCoordinate2D location = [mapView longitudeLatitudeForScreenPoint:point birdsEye:YES];
 	OSMRect viewCoord = [mapView screenLongitudeLatitude];
@@ -2630,31 +2635,30 @@ inline static CGFloat HitTestLineSegment(CLLocationCoordinate2D point, OSMSize m
 	OSMSize maxDegrees = { radius / pixelsPerDegree.width, radius / pixelsPerDegree.height };
 	const double NODE_BIAS = 0.5;	// make nodes appear closer so they can be selected
 
-	NSMutableSet * relations = [NSMutableSet new];
+	NSMutableSet * parentRelations = [NSMutableSet new];
 	for ( OsmBaseObject * object in objects ) {
 		if ( object.deleted )
 			continue;
+		if ( [ignoreList containsObject:object] )
+			continue;
+
 		if ( object.isNode ) {
 			OsmNode * node = (id)object;
-			if ( ![ignoreList containsObject:node] ) {
-				if ( testNodes || node.wayCount == 0 ) {
-					CGFloat dist = [self osmHitTestNode:node location:location maxDegrees:maxDegrees];
-					dist *= NODE_BIAS;
-					if ( dist <= 1.0 ) {
-						block( node, dist, 0 );
-						[relations addObjectsFromArray:node.parentRelations];
-					}
+			if ( testNodes || node.wayCount == 0 ) {
+				CGFloat dist = [self osmHitTestNode:node location:location maxDegrees:maxDegrees];
+				dist *= NODE_BIAS;
+				if ( dist <= 1.0 ) {
+					block( node, dist, 0 );
+					[parentRelations addObjectsFromArray:node.parentRelations];
 				}
 			}
 		} else if ( object.isWay ) {
 			OsmWay * way = (id)object;
-			if ( ![ignoreList containsObject:way] ) {
-				NSInteger seg = 0;
-				CGFloat dist = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
-				if ( dist <= 1.0 ) {
-					block( way, dist, seg );
-					[relations addObjectsFromArray:way.parentRelations];
-				}
+			NSInteger seg = 0;
+			CGFloat distToWay = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
+			if ( distToWay <= 1.0 ) {
+				block( way, distToWay, seg );
+				[parentRelations addObjectsFromArray:way.parentRelations];
 			}
 			if ( testNodes ) {
 				for ( OsmNode * node in way.nodes ) {
@@ -2664,48 +2668,46 @@ inline static CGFloat HitTestLineSegment(CLLocationCoordinate2D point, OSMSize m
 					dist *= NODE_BIAS;
 					if ( dist < 1.0 ) {
 						block( node, dist, 0 );
-						[relations addObjectsFromArray:node.parentRelations];
+						[parentRelations addObjectsFromArray:node.parentRelations];
 					}
 				}
 			}
 		} else if ( object.isRelation.isMultipolygon ) {
 			OsmRelation * relation = (id)object;
-			if ( ![ignoreList containsObject:relation] ) {
-				CGFloat bestDist = 10000.0;
-				for ( OsmMember * member in relation.members ) {
-					OsmWay * way = member.ref;
-					if ( [way isKindOfClass:[OsmWay class]] ) {
-						if ( ![ignoreList containsObject:way] ) {
-							if ( [member.role isEqualToString:@"inner"] || [member.role isEqualToString:@"outer"] ) {
-								NSInteger seg = 0;
-								CGFloat dist = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
-								if ( dist < bestDist )
-									bestDist = dist;
-							}
+			CGFloat bestDist = 10000.0;
+			for ( OsmMember * member in relation.members ) {
+				OsmWay * way = member.ref;
+				if ( [way isKindOfClass:[OsmWay class]] ) {
+					if ( ![ignoreList containsObject:way] ) {
+						if ( [member.role isEqualToString:@"inner"] || [member.role isEqualToString:@"outer"] ) {
+							NSInteger seg = 0;
+							CGFloat dist = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
+							if ( dist < bestDist )
+								bestDist = dist;
 						}
 					}
 				}
-				if ( bestDist <= 1.0 ) {
-					block( relation, bestDist, 0 );
-				}
+			}
+			if ( bestDist <= 1.0 ) {
+				block( relation, bestDist, 0 );
 			}
 		}
 	}
-	for ( OsmRelation * relation in relations ) {
+	for ( OsmRelation * relation in parentRelations ) {
 		// for non-multipolygon relations, like turn restrictions
 		block( relation, 1.0, 0 );
 	}
 }
 
 // default hit test when clicking on the map, or drag-connecting
-- (OsmBaseObject *)osmHitTest:(CGPoint)point radius:(CGFloat)radius testNodes:(BOOL)testNodes ignoreList:(NSArray<OsmBaseObject *> *)ignoreList segment:(NSInteger *)pSegment
+- (OsmBaseObject *)osmHitTest:(CGPoint)point radius:(CGFloat)radius isDragConnect:(BOOL)isDragConnect ignoreList:(NSArray<OsmBaseObject *> *)ignoreList segment:(NSInteger *)pSegment
 {
 	if ( self.hidden )
 		return nil;
 
 	__block CGFloat bestDist = 1000000;
 	NSMutableDictionary * best = [NSMutableDictionary new];
-	[EditorMapLayer osmHitTestEnumerate:point radius:radius mapView:_mapView objects:_shownObjects testNodes:testNodes ignoreList:ignoreList block:^(OsmBaseObject *obj, CGFloat dist, NSInteger segment) {
+	[EditorMapLayer osmHitTestEnumerate:point radius:radius mapView:_mapView objects:_shownObjects testNodes:isDragConnect ignoreList:ignoreList block:^(OsmBaseObject *obj, CGFloat dist, NSInteger segment) {
 		if ( dist < bestDist ) {
 			bestDist = dist;
 			[best removeAllObjects];
@@ -2719,21 +2721,32 @@ inline static CGFloat HitTestLineSegment(CLLocationCoordinate2D point, OSMSize m
 
 	OsmBaseObject * pick = nil;
 	if ( best.count > 1 ) {
-		if ( pick == nil && self.selectedRelation ) {
-			// pick a way that is a member of the relation if possible
-			for ( OsmMember * member in self.selectedRelation.members ) {
-				if ( best[member.ref] ) {
-					pick = member.ref;
+		if ( isDragConnect ) {
+			// prefer to connecct to a way in a relation over the relation itself, which is opposite what we do when selecting by tap
+			for ( OsmBaseObject * obj in best ) {
+				if ( !obj.isRelation ) {
+					pick = obj;
 					break;
 				}
 			}
-		}
-		if ( pick == nil && self.selectedPrimary == nil ) {
-			// nothing currently selected, so prefer relations
-			for ( OsmBaseObject * obj in best ) {
-				if ( obj.isRelation ) {
-					pick = obj;
-					break;
+		} else {
+			// performing selection by tap
+			if ( pick == nil && self.selectedRelation ) {
+				// pick a way that is a member of the relation if possible
+				for ( OsmMember * member in self.selectedRelation.members ) {
+					if ( best[member.ref] ) {
+						pick = member.ref;
+						break;
+					}
+				}
+			}
+			if ( pick == nil && self.selectedPrimary == nil ) {
+				// nothing currently selected, so prefer relations
+				for ( OsmBaseObject * obj in best ) {
+					if ( obj.isRelation ) {
+						pick = obj;
+						break;
+					}
 				}
 			}
 		}

src/Shared/EditorMapLayer.h
@@ -73,7 +73,7 @@ extern const double MinIconSizeInPixels;
 - (id)initWithMapView:(MapView *)mapView;
 - (void)didReceiveMemoryWarning;
 
-- (OsmBaseObject *)osmHitTest:(CGPoint)point radius:(CGFloat)radius testNodes:(BOOL)testNodes ignoreList:(NSArray<OsmBaseObject *> *)ignoreList segment:(NSInteger *)segment;
+- (OsmBaseObject *)osmHitTest:(CGPoint)point radius:(CGFloat)radius isDragConnect:(BOOL)isDragConnect ignoreList:(NSArray<OsmBaseObject *> *)ignoreList segment:(NSInteger *)segment;
 - (NSArray<OsmBaseObject *> *)osmHitTestMultiple:(CGPoint)point radius:(CGFloat)radius ;
 - (OsmNode *)osmHitTestNodeInSelectedWay:(CGPoint)point radius:(CGFloat)radius ;
 

src/Shared/MapView.m
@@ -2551,14 +2551,6 @@ NSString * ActionTitle( EDIT_ACTION action, BOOL abbrev )
 
 	OsmWay * way = _editorLayer.selectedWay;
 
-#if 0
-	// only do this checks if we want to be extra cautious
-	if ( node != way.nodes[0] && node != way.nodes.lastObject )
-		return nil;
-	if ( node.wayCount > 1 )
-		return nil;
-#endif
-
 	NSArray<OsmBaseObject *> * ignoreList = nil;
 	NSInteger index = [way.nodes indexOfObject:node];
 	NSArray<OsmWay *> * parentWays = node.wayCount == 1 ? @[ way ] : [_editorLayer.mapData waysContainingNode:node];
@@ -2575,10 +2567,10 @@ NSString * ActionTitle( EDIT_ACTION action, BOOL abbrev )
 		ignoreList = [way.nodes arrayByAddingObjectsFromArray:parentWays];
 	}
 	OsmBaseObject * hit = [_editorLayer osmHitTest:_pushpinView.arrowPoint
-											  radius:DragConnectHitTestRadius
-										   testNodes:YES
-										  ignoreList:ignoreList
-											 segment:segment];
+											radius:DragConnectHitTestRadius
+									 isDragConnect:YES
+										ignoreList:ignoreList
+										   segment:segment];
 	return hit;
 }
 
@@ -3616,7 +3608,7 @@ static NSString * const DisplayLinkPanning	= @"Panning";
 	} else {
 
 		// hit test anything
-		hit = [_editorLayer osmHitTest:point radius:DefaultHitTestRadius testNodes:NO ignoreList:nil segment:NULL];
+		hit = [_editorLayer osmHitTest:point radius:DefaultHitTestRadius isDragConnect:NO ignoreList:nil segment:NULL];
 		if ( hit ) {
 			if ( hit.isNode ) {
 				_editorLayer.selectedNode = (id)hit;

src/Shared/OsmMapData+Edit.m
@@ -243,7 +243,7 @@
 	};
 }
 
-#pragma mark canRemoveWayFromRelation
+#pragma mark canRemoveObject:fromRelation
 
 -(EditAction)canRemoveObject:(OsmBaseObject *)obj fromRelation:(OsmRelation *)relation error:(NSString **)error
 {
@@ -262,7 +262,7 @@
 	};
 }
 
-#pragma mark canReplaceNodeInWay
+#pragma mark canMergeNode:intoNode
 
 // used when dragging a node into another node
 -(EditActionReturnNode)canMergeNode:(OsmNode *)node1 intoNode:(OsmNode *)node2 error:(NSString **)error

src/iOS/Go Map!!.xcodeproj/project.pbxproj
@@ -744,9 +744,6 @@
 		02ACBA3916713CCB00BB4414 /* Go Map! */ = {
 			isa = PBXGroup;
 			children = (
-				64981F1D22667D7700178476 /* Extensions */,
-				64D74BF02253DF39004FFD20 /* Direction */,
-				64305F6C23E71F5700232BB9 /* URLParsing */,
 				02C3C73519A5D679003B6783 /* AerialEditViewController.h */,
 				02C3C73619A5D679003B6783 /* AerialEditViewController.m */,
 				02C3C73219A5A40C003B6783 /* AerialListViewController.h */,
@@ -765,12 +762,16 @@
 				0230558A19B931B20041B151 /* CustomPresetController.m */,
 				024B4CA019BBE65F00BBA60B /* CustomPresetListViewController.h */,
 				024B4CA119BBE67400BBA60B /* CustomPresetListViewController.m */,
+				64D74BF02253DF39004FFD20 /* Direction */,
 				0225A36819B63EFE008B63BA /* DisplayViewController.h */,
 				0225A36919B63F0D008B63BA /* DisplayViewController.m */,
+				64981F1D22667D7700178476 /* Extensions */,
 				028E0C0A20B2182100CEC71F /* FilterObjectsViewController.h */,
 				028E0C0B20B2182100CEC71F /* FilterObjectsViewController.m */,
 				021D4E49194E0BCB002154B3 /* FpsLabel.h */,
 				021D4E4A194E0BCB002154B3 /* FpsLabel.m */,
+				6463E3C923F948FE00E4F6ED /* GeekbenchScoreProvider.h */,
+				6463E3CA23F948FE00E4F6ED /* GeekbenchScoreProvider.m */,
 				02F04AD01BC4D3B50044FD95 /* GpxConfigureViewController.h */,
 				02F04AD11BC4D3B50044FD95 /* GpxConfigureViewController.m */,
 				0219563016DDC26E00074D15 /* GpxViewController.h */,
@@ -790,6 +791,7 @@
 				02ACBA4B16713CCB00BB4414 /* MainStoryboard.storyboard */,
 				02ACBA5116713CCB00BB4414 /* MapViewController.h */,
 				02ACBA5216713CCB00BB4414 /* MapViewController.m */,
+				64C968442261ED3100351C0C /* Media.xcassets */,
 				022ABC9F20C1AACF002D4977 /* MultilineTableViewCell.h */,
 				022ABCA020C1AACF002D4977 /* MultilineTableViewCell.m */,
 				02BED98C168BF87100357B94 /* NearbyMappersViewController.h */,
@@ -827,12 +829,10 @@
 				02BF745320793F0A0028ED6A /* TurnRestrictController.m */,
 				02F373F716822C2C004614D4 /* UploadViewController.h */,
 				02F373F816822C2C004614D4 /* UploadViewController.m */,
+				64305F6C23E71F5700232BB9 /* URLParsing */,
 				02E8436E19B407C40044AC1D /* WebPageView.xib */,
 				02BED988168B6F2F00357B94 /* WebPageViewController.h */,
 				02BED989168B6F2F00357B94 /* WebPageViewController.m */,
-				64C968442261ED3100351C0C /* Media.xcassets */,
-				6463E3C923F948FE00E4F6ED /* GeekbenchScoreProvider.h */,
-				6463E3CA23F948FE00E4F6ED /* GeekbenchScoreProvider.m */,
 			);
 			name = "Go Map!";
 			sourceTree = "<group>";

commit 85e81c868bc9b1f9fc09de96063eb5f5978b1466

Fixes a bug introduced by 3e24d761478e1bb1e44df33492c47242b1f04344 plus a little code cleanup.

src/Shared/EditorMapLayer.m
@@ -2283,12 +2283,7 @@ static BOOL VisibleSizeLessStrict( OsmBaseObject * obj1, OsmBaseObject * obj2 )
 	}
 
 	// sort from big to small objects
-#if 0
-	[objects partialSortK:2*objectLimit+1 compare:VisibleSizeLessStrict];
-#else
 	[objects partialSortOsmObjectVisibleSize:2*objectLimit+1];
-#endif
-
 
 	// adjust the list of objects so that we get all or none of the same type
 	if ( objects.count > objectLimit ) {
@@ -2319,19 +2314,12 @@ static BOOL VisibleSizeLessStrict( OsmBaseObject * obj1, OsmBaseObject * obj2 )
 				lastIndex = objectLimit - removeCount;
 			}
 		}
-		// DLog( @"added %ld same", (long)lastIndex - objectLimit);
 		objectLimit = lastIndex;
 
 		// remove unwanted objects
 		NSIndexSet * range = [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(objectLimit,objects.count-objectLimit)];
 		[objects removeObjectsAtIndexes:range];
 	}
-
-#if 0
-	for ( OsmBaseObject * o in objects ) {
-		NSLog(@"%ld -> %@\n", (long)o->renderPriorityCached, o );
-	}
-#endif
 
 	// sometimes there are way too many address nodes that clog up the view, so limit those items specifically
 	objectLimit = objects.count;
@@ -2629,26 +2617,28 @@ inline static CGFloat HitTestLineSegment(CLLocationCoordinate2D point, OSMSize m
 	for ( OsmBaseObject * object in objects ) {
 		if ( object.deleted )
 			continue;
-		if ( [ignoreList containsObject:object] )
-			continue;
 
 		if ( object.isNode ) {
 			OsmNode * node = (id)object;
-			if ( testNodes || node.wayCount == 0 ) {
-				CGFloat dist = [self osmHitTestNode:node location:location maxDegrees:maxDegrees];
-				dist *= NODE_BIAS;
-				if ( dist <= 1.0 ) {
-					block( node, dist, 0 );
-					[parentRelations addObjectsFromArray:node.parentRelations];
+			if ( ![ignoreList containsObject:node] ) {
+				if ( testNodes || node.wayCount == 0 ) {
+					CGFloat dist = [self osmHitTestNode:node location:location maxDegrees:maxDegrees];
+					dist *= NODE_BIAS;
+					if ( dist <= 1.0 ) {
+						block( node, dist, 0 );
+						[parentRelations addObjectsFromArray:node.parentRelations];
+					}
 				}
 			}
 		} else if ( object.isWay ) {
 			OsmWay * way = (id)object;
-			NSInteger seg = 0;
-			CGFloat distToWay = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
-			if ( distToWay <= 1.0 ) {
-				block( way, distToWay, seg );
-				[parentRelations addObjectsFromArray:way.parentRelations];
+			if ( ![ignoreList containsObject:way] ) {
+				NSInteger seg = 0;
+				CGFloat distToWay = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
+				if ( distToWay <= 1.0 ) {
+					block( way, distToWay, seg );
+					[parentRelations addObjectsFromArray:way.parentRelations];
+				}
 			}
 			if ( testNodes ) {
 				for ( OsmNode * node in way.nodes ) {
@@ -2664,22 +2654,24 @@ inline static CGFloat HitTestLineSegment(CLLocationCoordinate2D point, OSMSize m
 			}
 		} else if ( object.isRelation.isMultipolygon ) {
 			OsmRelation * relation = (id)object;
-			CGFloat bestDist = 10000.0;
-			for ( OsmMember * member in relation.members ) {
-				OsmWay * way = member.ref;
-				if ( [way isKindOfClass:[OsmWay class]] ) {
-					if ( ![ignoreList containsObject:way] ) {
-						if ( [member.role isEqualToString:@"inner"] || [member.role isEqualToString:@"outer"] ) {
-							NSInteger seg = 0;
-							CGFloat dist = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
-							if ( dist < bestDist )
-								bestDist = dist;
+			if ( ![ignoreList containsObject:relation] ) {
+				CGFloat bestDist = 10000.0;
+				for ( OsmMember * member in relation.members ) {
+					OsmWay * way = member.ref;
+					if ( [way isKindOfClass:[OsmWay class]] ) {
+						if ( ![ignoreList containsObject:way] ) {
+							if ( [member.role isEqualToString:@"inner"] || [member.role isEqualToString:@"outer"] ) {
+								NSInteger seg = 0;
+								CGFloat dist = [self osmHitTestWay:way location:location maxDegrees:maxDegrees segment:&seg];
+								if ( dist < bestDist )
+									bestDist = dist;
+							}
 						}
 					}
 				}
-			}
-			if ( bestDist <= 1.0 ) {
-				block( relation, bestDist, 0 );
+				if ( bestDist <= 1.0 ) {
+					block( relation, bestDist, 0 );
+				}
 			}
 		}
 	}