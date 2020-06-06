37fda45064b510328849c03ce01c78316c1146a3
Bryce Cogswell <bryceco@yahoo.com>
2020-06-05 17:10:46 -0700

    Fix for screen rotation for Show Touches

--- src/iOS/MyApplication.m
+++ src/iOS/MyApplication.m
@@ -44,8 +44,13 @@ static const CGFloat TOUCH_RADIUS = 22;
 	for ( UITouch * touch in event.allTouches ) {
 
 		CGPoint pos = [touch locationInView:nil];
-		// if we double-tap then then second tap will be captured by our own window
+		// if we double-tap then the second tap will be captured by our own window
 		pos = [touch.window convertPoint:pos toWindow:nil];
+
+		if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
+			// Translate coordinates in case screen is rotated. On iPad the tranform is done for us.
+			pos = [UIScreen.mainScreen.coordinateSpace convertPoint:pos toCoordinateSpace:UIScreen.mainScreen.fixedCoordinateSpace];
+		}
 
 		if ( touch.phase == UITouchPhaseBegan ) {
 			UIWindow * win = [[UIWindow alloc] initWithFrame:[self rectForTouchPosition:pos]];
