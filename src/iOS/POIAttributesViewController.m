39d06f1db441d44873a528035ef4b27e703638bf
Bryce Cogswell <bryceco@yahoo.com>
2020-06-10 22:36:06 -0700

    Show length of ways under Attributes

--- src/iOS/POIAttributesViewController.m
+++ src/iOS/POIAttributesViewController.m
@@ -27,12 +27,17 @@ enum {
 	ROW_UID,
 	ROW_MODIFIED,
 	ROW_VERSION,
-	ROW_CHANGESET
+	ROW_CHANGESET,
 };
 
 @implementation POIAttributesViewController
 
-const NSInteger kCoordinateSection = 1;
+enum {
+	SECTION_METADATA = 0,
+	SECTION_NODE_LATLON = 1,
+	SECTION_WAY_EXTRA = 1,
+	SECTION_WAY_NODES = 2,
+};
 
 - (void)viewDidLoad
 {
@@ -57,24 +62,27 @@ const NSInteger kCoordinateSection = 1;
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-	AppDelegate * appDelegate = [AppDelegate getAppDelegate];
-	OsmBaseObject * object = appDelegate.mapView.editorLayer.selectedPrimary;
-	return object.isNode || object.isWay ? 2 : 1;
+	OsmBaseObject * object = AppDelegate.getAppDelegate.mapView.editorLayer.selectedPrimary;
+	return object.isNode ? 2 : object.isWay ? 3 : 1;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	if ( section == 0 )
-		return 6;
+	OsmBaseObject * object = AppDelegate.getAppDelegate.mapView.editorLayer.selectedPrimary;
 
-	AppDelegate * appDelegate = [AppDelegate getAppDelegate];
-	OsmBaseObject * object = appDelegate.mapView.editorLayer.selectedPrimary;
-	if ( object.isNode )
-		return 1;	// longitude/latitude
-	if ( object.isWay )
-		return object.isWay.nodes.count;	// all nodes
-	if ( object.isRelation )
-		return 0;
+	if ( section == SECTION_METADATA ) {
+		return 6;
+	}
+	if ( object.isNode ) {
+		if ( section == SECTION_NODE_LATLON )
+			return 1;	// longitude/latitude
+	} else if ( object.isWay ) {
+		if ( section == SECTION_WAY_EXTRA ) {
+			return 1;
+		} else if ( section == SECTION_WAY_NODES ) {
+			return object.isWay.nodes.count;	// all nodes
+		}
+	}
 	return 0;
 }
 
@@ -86,7 +94,7 @@ const NSInteger kCoordinateSection = 1;
 	AttributeCustomCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
 	cell.accessoryType	= UITableViewCellAccessoryNone;
 
-	if ( indexPath.section == 0 ) {
+	if ( indexPath.section == SECTION_METADATA ) {
 
 		switch ( indexPath.row ) {
 			case ROW_IDENTIFIER:
@@ -121,18 +129,20 @@ const NSInteger kCoordinateSection = 1;
 				assert(NO);
 		}
 
-	} else if (indexPath.section == kCoordinateSection) {
-
-		if ( object.isNode ) {
+	} else if ( object.isNode ) {
+		if ( indexPath.section == SECTION_NODE_LATLON ) {
 			OsmNode * node = object.isNode;
-			switch ( indexPath.row ) {
-				case 0:
-					cell.title.text = NSLocalizedString(@"Lat/Lon",nil);
-					cell.value.text = [NSString stringWithFormat:@"%f,%f", node.lat, node.lon];
-				default:
-					break;
-			}
-		} else if ( object.isWay ) {
+			cell.title.text = NSLocalizedString(@"Lat/Lon",nil);
+			cell.value.text = [NSString stringWithFormat:@"%f,%f", node.lat, node.lon];
+		}
+	} else if ( object.isWay ) {
+		if ( indexPath.section == SECTION_WAY_EXTRA ) {
+			double len = object.isWay.lengthInMeters;
+			cell.title.text = NSLocalizedString(@"Length",nil);
+			cell.value.text = len >= 10 ? [NSString stringWithFormat:NSLocalizedString(@"%.0f meters",nil), len]
+										: [NSString stringWithFormat:NSLocalizedString(@"%.1f meters",nil), len];
+			cell.accessoryType = UITableViewCellAccessoryNone;
+		} else if ( indexPath.section == SECTION_WAY_NODES ) {
 			OsmWay * way = object.isWay;
 			OsmNode * node = way.nodes[ indexPath.row ];
 			cell.title.text = NSLocalizedString(@"Node",nil);
@@ -142,10 +152,10 @@ const NSInteger kCoordinateSection = 1;
 			else
 				name = node.ident.stringValue;
 			cell.value.text = name;
-		} else {
-			// shouldn't be here
-			assert(NO);
 		}
+	} else {
+		// shouldn't be here
+		assert(NO);
 	}
 	// do extra work so keyboard won't display if they select a value
 	UITextField * value = cell.value;
@@ -165,8 +175,7 @@ const NSInteger kCoordinateSection = 1;
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    AppDelegate * appDelegate = [AppDelegate getAppDelegate];
-    OsmBaseObject * object = appDelegate.mapView.editorLayer.selectedPrimary;
+	OsmBaseObject * object = AppDelegate.getAppDelegate.mapView.editorLayer.selectedPrimary;
     if ( object == nil ) {
         return;
     }
@@ -194,13 +203,15 @@ const NSInteger kCoordinateSection = 1;
     }
 }
 
-- (BOOL)tableView:(UITableView *)tableView shouldShowMenuForRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Allow the user to copy the latitude/longitude.
-    return indexPath.section == kCoordinateSection;
+- (BOOL)tableView:(UITableView *)tableView shouldShowMenuForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Allow the user to copy the latitude/longitude
+    return indexPath.section == SECTION_NODE_LATLON;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canPerformAction:(SEL)action forRowAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-    if (indexPath.section == kCoordinateSection && action == @selector(copy:)) {
+- (BOOL)tableView:(UITableView *)tableView canPerformAction:(SEL)action forRowAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender
+{
+    if (indexPath.section == SECTION_NODE_LATLON && action == @selector(copy:)) {
         // Allow users to copy latitude/longitude.
         return YES;
     }
@@ -208,7 +219,8 @@ const NSInteger kCoordinateSection = 1;
     return NO;
 }
 
-- (void)tableView:(UITableView *)tableView performAction:(SEL)action forRowAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+- (void)tableView:(UITableView *)tableView performAction:(SEL)action forRowAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender
+{
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     if (![cell isKindOfClass:[AttributeCustomCell class]]) {
         // For cells other than `AttributeCustomCell`, we don't know how to get the value.
@@ -217,7 +229,7 @@ const NSInteger kCoordinateSection = 1;
     
     AttributeCustomCell *customCell = (AttributeCustomCell *)cell;
     
-    if (indexPath.section == kCoordinateSection && action == @selector(copy:)) {
+    if (indexPath.section == SECTION_NODE_LATLON && action == @selector(copy:)) {
         [UIPasteboard.generalPasteboard setString:customCell.value.text];
     }
 }
