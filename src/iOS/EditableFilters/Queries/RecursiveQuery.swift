bb80ed0f1b7168be6105ed795dff67f2fd97af51
wtimme <wtimme@users.noreply.github.com>
2020-09-10 18:59:24 +0200

    Add `RecursiveQuery`

---
+++ src/iOS/EditableFilters/Queries/RecursiveQuery.swift
@@ -0,0 +1,37 @@
+//
+//  RecursiveQuery.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 5/1/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+struct RecursiveQuery {
+    enum Logical: String, CaseIterable {
+        case and, or
+    }
+
+    let logical: Logical
+    let queries: [BaseObjectMatching]
+
+    init(logical: Logical = .or, queries: [BaseObjectMatching]) {
+        self.logical = logical
+        self.queries = queries
+    }
+}
+
+extension RecursiveQuery: BaseObjectMatching {
+    func matches(_ object: OsmBaseObject) -> Bool {
+        guard queries.count > 0 else {
+            // Without child queries, the recursive query does not match any object.
+            return false
+        }
+
+        switch logical {
+        case .and:
+            return queries.first { !$0.matches(object) } == nil
+        case .or:
+            return queries.first { $0.matches(object) } != nil
+        }
+    }
+}
---
+++ src/iOS/GoMapTests/EditableFilters/Queries/RecursiveQueryTestCase.swift
@@ -0,0 +1,77 @@
+//
+//  RecursiveQueryTestCase.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 5/1/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+@testable import Go_Map__
+import XCTest
+
+class RecursiveQueryTestCase: XCTestCase {
+    func testRecursiveQueryWithNoQueriesShouldReturnFalse() {
+        RecursiveQuery.Logical.allCases.forEach { logical in
+            let query = RecursiveQuery(logical: logical, queries: [])
+
+            XCTAssertFalse(query.matches(OsmBaseObject()))
+        }
+    }
+
+    func testRecursiveQueryWithLogicalAndAndAllQueriesReturningTrueShouldReturnTrue() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .and, queries: [firstMatchMock, secondMatchMock])
+
+        firstMatchMock.doesMatch = true
+        secondMatchMock.doesMatch = true
+
+        XCTAssertTrue(query.matches(OsmBaseObject()))
+    }
+
+    func testRecursiveQueryWithLogicalAndAndOneQueryReturningFalseShouldReturnFalse() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .and, queries: [firstMatchMock, secondMatchMock])
+
+        firstMatchMock.doesMatch = true
+        secondMatchMock.doesMatch = false
+
+        XCTAssertFalse(query.matches(OsmBaseObject()))
+    }
+
+    func testRecursiveQueryWithLogicalOrAndAllQueriesReturningFalseShouldReturnFalse() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .or, queries: [firstMatchMock, secondMatchMock])
+
+        firstMatchMock.doesMatch = false
+        secondMatchMock.doesMatch = false
+
+        XCTAssertFalse(query.matches(OsmBaseObject()))
+    }
+
+    func testRecursiveQueryWithLogicalOrAndOneQueryReturningTrueShouldReturnTrue() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .or, queries: [firstMatchMock, secondMatchMock])
+
+        firstMatchMock.doesMatch = false
+        secondMatchMock.doesMatch = true
+
+        XCTAssertTrue(query.matches(OsmBaseObject()))
+    }
+
+    func testRecursiveQueryWithLogicalOrShouldStopAskingForMatchesWhenAMatchWasFound() {
+        let firstMatchMock = BaseObjectMatcherMock()
+        let secondMatchMock = BaseObjectMatcherMock()
+        let query = RecursiveQuery(logical: .or, queries: [firstMatchMock, secondMatchMock])
+
+        firstMatchMock.doesMatch = true
+
+        XCTAssertTrue(query.matches(OsmBaseObject()))
+
+        XCTAssertNil(secondMatchMock.object,
+                     "Since the first query yielded a positive result, the second one should not have been evaluated.")
+    }
+}
