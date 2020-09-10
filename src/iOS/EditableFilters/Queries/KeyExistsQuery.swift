e6610476b2c60b3df792f76a4b30f5def913931a
wtimme <wtimme@users.noreply.github.com>
2020-09-10 19:05:41 +0200

    Add `KeyExistsQuery`

---
+++ src/iOS/EditableFilters/Queries/KeyExistsQuery.swift
@@ -0,0 +1,29 @@
+//
+//  KeyExistsQuery.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 5/1/19.
+//  Copyright © 2019 Bryce. All rights reserved.
+//
+
+struct KeyExistsQuery {
+    let key: String
+    let isNegated: Bool
+
+    init(key: String, isNegated: Bool = false) {
+        self.key = key
+        self.isNegated = isNegated
+    }
+}
+
+extension KeyExistsQuery: BaseObjectMatching {
+    func matches(_ object: OsmBaseObject) -> Bool {
+        let keyExists = object.tags?.keys.contains(key) ?? false
+
+        if isNegated {
+            return !keyExists
+        } else {
+            return keyExists
+        }
+    }
+}
