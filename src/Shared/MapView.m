a1b0ae665f8284f0e84d9b15e01de0c42147e8e5
Bryce Cogswell <bryceco@yahoo.com>
2020-09-16 14:53:15 -0700

    Remove unnecessary cast

--- src/Shared/MapView.m
+++ src/Shared/MapView.m
@@ -695,7 +695,7 @@ const CGFloat kEditControlCornerRadius = 4;
 		[UIView animateWithDuration:0.35 animations:^{
 			_flashLabel.alpha = 0.0;
 		} completion:^(BOOL finished){
-			if ( finished && ((CALayer *)_flashLabel.layer.presentationLayer).opacity == 0.0 ) {
+			if ( finished && _flashLabel.layer.presentationLayer.opacity == 0.0 ) {
 				_flashLabel.hidden = YES;
 			}
 		}];
