80093cf490fb01a0efc7df9e509cbf646161a1cf
Bryce Cogswell <bryceco@yahoo.com>
2021-10-13 19:07:07 -0700

    Obfuscates mapbox token

--- src/Shared/Tiles/TileServer.swift
+++ src/Shared/Tiles/TileServer.swift
@@ -247,7 +247,18 @@ final class TileServer {
 		attribIcon: nil,
 		attribUrl: "")
 
-	static let mapboxToken = Bundle.main.object(forInfoDictionaryKey: "MBXLocatorToken") as? String ?? ""
+	static let mapboxToken: String = {
+		guard let a2 = Bundle.main.object(forInfoDictionaryKey: "MBX") as? [Int],
+			  a2.count % 2 == 0
+		else {
+			return ""
+		}
+		return a2.indices
+			.compactMap{ $0 % 2 == 0 ? (64-a2[$0],64-a2[$0+1]) : nil }
+			.sorted(by: {v1,v2 in return v1.1 < v2.1})
+			.map{ "\(Character(UnicodeScalar($0.0)!))" }
+			.joined(separator: "")
+	}()
 	static let mapboxLocator = TileServer(
 		withName: "Mapbox Locator",
 		identifier: MAPBOX_LOCATOR_IDENTIFIER,
