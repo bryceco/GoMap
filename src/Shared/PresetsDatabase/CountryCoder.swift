9861219e0f51666ce07533fc4bc1ff7f6632f18a
Bryce Cogswell <bryceco@yahoo.com>
2022-11-21 21:16:03 -0800

    Update CountryCoder to return lists of region strings

--- src/Shared/PresetsDatabase/CountryCoder.swift
+++ src/Shared/PresetsDatabase/CountryCoder.swift
@@ -1,21 +1,27 @@
 import UIKit
 
-struct Region {
+public struct CountryCoderRegion {
 	let country: String? // Country code
 	let iso1A2: String? // ISO 3166-1 alpha-2 code
 	let iso1A3: String? // ISO 3166-1 alpha-3 code
 	let iso1N3: String? // ISO 3166-1 numeric-3 code
 	let m49: String? // UN M49 code
+	let groups: [String]
 
 	let bezierPath: UIBezierPath
 	let boundingBox: CGRect
 
-	init(country: String?, iso1A2: String?, iso1A3: String?, iso1N3: String?, m49: String?, bezierPath: UIBezierPath) {
+	init(country: String?,
+		 iso1A2: String?, iso1A3: String?, iso1N3: String?, m49: String?,
+		 groups: [String],
+		 bezierPath: UIBezierPath)
+	{
 		self.country = country
 		self.iso1A2 = iso1A2
 		self.iso1A3 = iso1A3
 		self.iso1N3 = iso1N3
 		self.m49 = m49
+		self.groups = groups
 		self.bezierPath = bezierPath
 		boundingBox = bezierPath.bounds
 	}
@@ -26,6 +32,13 @@ struct Region {
 			code == iso1A3 ||
 			code == iso1N3 ||
 			code == m49
+	}
+
+	func addSynonyms(to list:inout [String]) {
+		if let s = iso1A2 { list.append(s) }
+		if let s = iso1A3 { list.append(s) }
+		if let s = m49 { list.append(s) }
+		if let s = iso1N3 { list.append(s) }
 	}
 
 	private static func addPoints(_ points: [[Double]], to path: UIBezierPath) {
@@ -61,8 +74,8 @@ struct Region {
 public final class CountryCoder {
 	public static let shared = CountryCoder()
 
-	let regionList: [Region]
-	let regionDict: [String: Region]
+	let regionList: [CountryCoderRegion]
+	let regionDict: [String: CountryCoderRegion]
 
 	private init() {
 		guard let path = Bundle.main.resourcePath,
@@ -77,7 +90,7 @@ public final class CountryCoder {
 			fatalError()
 		}
 
-		var regions: [Region] = []
+		var regions: [CountryCoderRegion] = []
 
 		for featureAny in features {
 			guard let feature = featureAny as? [String: Any],
@@ -96,25 +109,27 @@ public final class CountryCoder {
 			let iso1A3 = properties["iso1A3"] as? String
 			let iso1N3 = properties["iso1N3"] as? String
 			let m49 = properties["m49"] as? String
+			let groups = properties["groups"] as? [String] ?? []
 
 			switch geometry["type"] as? String {
 			case "MultiPolygon":
 				guard let mp = geometry["coordinates"] as? [[[[Double]]]] else {
 					fatalError()
 				}
-				regions.append(Region(country: country,
+				regions.append(CountryCoderRegion(country: country,
 				                      iso1A2: iso1A2,
 				                      iso1A3: iso1A3,
 				                      iso1N3: iso1N3,
 				                      m49: m49,
-				                      bezierPath: Region.geometryAsBezier(mp)))
+									  groups: groups,
+				                      bezierPath: CountryCoderRegion.geometryAsBezier(mp)))
 			default:
 				fatalError()
 			}
 		}
 		regionList = regions
 
-		var dict = [String: Region]()
+		var dict = [String: CountryCoderRegion]()
 		for r in regions {
 			if let s = r.country { dict[s] = r }
 			if let s = r.iso1A2 { dict[s] = r }
@@ -139,5 +154,40 @@ public final class CountryCoder {
 	func region(_ code: String, contains latLon: LatLon) -> Bool {
 		let cgPoint = CGPoint(x: latLon.lon, y: latLon.lat)
 		return region(code, contains: cgPoint)
+	}
+
+	private func addRegion(_ region: CountryCoderRegion, to list:inout [String]) {
+		region.addSynonyms(to: &list)
+		for group in region.groups {
+			if let r = regionDict[group] {
+				addRegion(r, to: &list)
+			}
+		}
+	}
+
+	func regionsAt(_ loc: LatLon) -> [CountryCoderRegion] {
+		var list: [CountryCoderRegion] = []
+		let cgPoint = CGPoint(OSMPoint(loc))
+		for region in regionList {
+			if region.boundingBox.contains(cgPoint),
+			   region.bezierPath.contains(cgPoint)
+			{
+				list.append(region)
+				for group in region.groups {
+					if let r = regionDict[group] {
+						list.append(r)
+					}
+				}
+			}
+		}
+		return list
+	}
+
+	static func regionsStringsForRegions(_ list: [CountryCoderRegion]) -> [String] {
+		var result: [String] = []
+		for region in list {
+			region.addSynonyms(to: &result)
+		}
+		return result
 	}
 }
