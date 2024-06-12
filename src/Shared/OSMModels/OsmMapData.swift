bryceco/GoMap

commit 7aac560e5fde4a3f2c8d182787617280a463698c

Use the appropriate server URL for calls to taginfo, nominatim and attributes queries

src/Shared/OSMModels/OsmMapData.swift
@@ -16,9 +16,6 @@ typealias EditActionWithNode = (OsmNode) -> Void
 typealias EditActionReturnWay = () -> OsmWay
 typealias EditActionReturnNode = () -> OsmNode
 
-// "https://api.openstreetmap.org/"
-var OSM_API_URL = ""
-
 final class OsmUserStatistics {
 	var user = ""
 	var lastEdit: Date!
@@ -75,43 +72,11 @@ final class OsmMapData: NSObject, NSSecureCoding {
 
 	// MARK: Utility
 
-	func serverNameCanonicalized(_ hostname: String) -> String {
-		var hostname = hostname
-		hostname = hostname.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-
-		if hostname.hasPrefix("http://") || hostname.hasPrefix("https://") {
-			// great
-		} else {
-			hostname = "https://" + hostname
-		}
-
-		while hostname.hasSuffix("/") {
-			hostname = String(hostname.dropLast())
-		}
-		hostname = hostname + "/"
-
-		return hostname
-	}
-
-	func setServer(_ hostname: String) {
-		let hostname = serverNameCanonicalized(hostname)
-
-		if OSM_API_URL == hostname {
-			// no change
-			return
-		}
-
-		if OSM_API_URL.count != 0 {
+	func setServer(_ host: OsmServer) {
+		if OSM_SERVER.apiURL.count != 0 {
 			// get rid of old data before connecting to new server
 			purgeSoft()
 		}
-
-		UserPrefs.shared.osmServerUrl.value = hostname
-		OSM_API_URL = hostname
-	}
-
-	func getServer() -> String {
-		return OSM_API_URL
 	}
 
 	func setupPeriodicSaveTimer() {
@@ -685,7 +650,7 @@ final class OsmMapData: NSObject, NSSecureCoding {
 			progress.progressIncrement()
 
 			let rc = query.rect
-			let url = OSM_API_URL +
+			let url = OSM_SERVER.apiURL +
 				"api/0.6/map?bbox=\(rc.origin.x),\(rc.origin.y),\(rc.origin.x + rc.size.width),\(rc.origin.y + rc.size.height)"
 
 			OsmDownloader.osmData(forUrl: url, completion: { result in
@@ -855,7 +820,7 @@ final class OsmMapData: NSObject, NSSecureCoding {
 		retries: Int,
 		completion: @escaping (_ error: Error?) -> Void)
 	{
-		let url2 = OSM_API_URL + "api/0.6/changeset/\(changesetID)/upload"
+		let url2 = OSM_SERVER.apiURL + "api/0.6/changeset/\(changesetID)/upload"
 		putRequest(url: url2, method: "POST", xml: xmlChanges) { [self] result in
 
 			switch result {
@@ -884,7 +849,7 @@ final class OsmMapData: NSObject, NSSecureCoding {
 					   let objType = objType
 					{
 						let objType = (objType as String).lowercased()
-						var url3 = OSM_API_URL + "api/0.6/\(objType)/\(objId)"
+						var url3 = OSM_SERVER.apiURL + "api/0.6/\(objType)/\(objId)"
 						if objType == "way" || objType == "relation" {
 							url3 = url3 + "/full"
 						}
@@ -968,7 +933,7 @@ final class OsmMapData: NSObject, NSSecureCoding {
 
 				updateSql(sqlUpdate)
 
-				let url3 = OSM_API_URL + "api/0.6/changeset/\(changesetID)/close"
+				let url3 = OSM_SERVER.apiURL + "api/0.6/changeset/\(changesetID)/close"
 				putRequest(url: url3, method: "PUT", xml: nil) { result in
 					switch result {
 					case .success:
@@ -1106,7 +1071,7 @@ final class OsmMapData: NSObject, NSSecureCoding {
 			tags["locale"] = locale
 		}
 		if let xmlCreate = OsmXmlGenerator.createXml(withType: "changeset", tags: tags) {
-			let url = OSM_API_URL + "api/0.6/changeset/create"
+			let url = OSM_SERVER.apiURL + "api/0.6/changeset/create"
 			putRequest(url: url, method: "PUT", xml: xmlCreate) { result in
 				switch result {
 				case let .failure(error):
@@ -1202,8 +1167,7 @@ final class OsmMapData: NSObject, NSSecureCoding {
 	// MARK: Init/Save/Restore
 
 	func initCommon() {
-		let server = UserPrefs.shared.osmServerUrl.value ?? "https://api.openstreetmap.org/"
-		setServer(server)
+		setServer(OSM_SERVER)
 		setupPeriodicSaveTimer()
 	}
 

src/Shared/OSMModels/OsmServer.swift
@@ -8,43 +8,119 @@
 
 import Foundation
 
-enum OsmServerID {
-	case osm
-	case dev
-	case ohm
-	case ogf
+// "https://api.openstreetmap.org/"
+var OSM_SERVER: OsmServer = {
+	guard let serverUrl = UserPrefs.shared.osmServerUrl.value else {
+		return OsmServerList.first!
+	}
+	return OsmServer.serverForUrl(serverUrl)
+}() {
+	didSet {
+		if oldValue.apiURL == OSM_SERVER.apiURL {
+			return
+		}
+		UserPrefs.shared.osmServerUrl.value = OSM_SERVER.apiURL
+		AppDelegate.shared.mapView.editorLayer.mapData.setServer(OSM_SERVER)
+	}
 }
 
 struct OsmServer {
-	let ident: OsmServerID // persistent identifier
 	let fullName: String // friendly name of the server
 	let apiURL: String // used for API connections
 	let authURL: String // used for OAuth connections
+	let nominatimUrl: String
 	let client_id: String // used for OAuth connections
+
+	var queryURL: String { // used for things like "www.openstreetmap.org/user/bryceco"
+		return authURL
+	}
+
+	init(fullName: String,
+	     apiHost: String,
+	     authHost: String,
+	     nominatimHost: String,
+	     client_id: String)
+	{
+		self.fullName = fullName
+		apiURL = "https://" + apiHost + "/"
+		authURL = "https://" + authHost + "/"
+		nominatimUrl = "https://" + nominatimHost + "/"
+		self.client_id = client_id
+	}
+
+	init(apiUrl: String) {
+		fullName = ""
+		apiURL = apiUrl
+		authURL = ""
+		nominatimUrl = ""
+		client_id = ""
+	}
+
+	var taginfoUrl: String {
+		if var comps = URLComponents(string: authURL),
+		   let parts = comps.host?.split(separator: ".")
+		{
+			let suffix = parts.dropFirst()
+			let newParts = ["taginfo"] + suffix
+			comps.host = newParts.joined(separator: ".")
+			if let url = comps.url {
+				return url.absoluteString
+			}
+		}
+		// This won't work but there's nothing we can do about it
+		return authURL
+	}
+
+	static func serverNameCanonicalized(_ hostname: String) -> String {
+		var hostname = hostname
+		hostname = hostname.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+		if hostname.hasPrefix("http://") || hostname.hasPrefix("https://") {
+			// great
+		} else {
+			hostname = "https://" + hostname
+		}
+
+		while hostname.hasSuffix("/") {
+			hostname = String(hostname.dropLast())
+		}
+		hostname = hostname + "/"
+
+		return hostname
+	}
+
+	static func serverForUrl(_ url: String) -> OsmServer {
+		let url = OsmServer.serverNameCanonicalized(url)
+		if let server = OsmServerList.first(where: { $0.apiURL == url }) {
+			// it's one of our built-in servers
+			return server
+		}
+		return OsmServer(apiUrl: url)
+	}
 }
 
 let OsmServerList = [
-	OsmServer(ident: .osm,
-	          fullName: "OpenStreetMap",
-	          apiURL: "https://api.openstreetmap.org/",
-	          authURL: "https://www.openstreetmap.org/",
+	OsmServer(fullName: "OpenStreetMap",
+	          apiHost: "api.openstreetmap.org",
+	          authHost: "www.openstreetmap.org",
+	          nominatimHost: "nominatim.openstreetmap.org",
 	          client_id: "mzRn5Z-X0nSptHgA3o5g30HeaaljTXfv0GMOLhmwqeo"),
 
-	OsmServer(ident: .dev,
-	          fullName: "OSM Dev Server",
-	          apiURL: "https://api06.dev.openstreetmap.org/",
-	          authURL: "https://master.apis.dev.openstreetmap.org/",
+	OsmServer(fullName: "OSM Dev Server",
+	          apiHost: "api06.dev.openstreetmap.org",
+	          authHost: "master.apis.dev.openstreetmap.org",
+	          nominatimHost: "nominatim.openstreetmap.org",
 	          client_id: "SGsePsBukg7xPkIaqNIQlAgiAa3vjauIFkbcsPXB2Tg"),
 
-	OsmServer(ident: .ohm,
-	          fullName: "OpenHistoricalMap",
-	          apiURL: "https://www.openhistoricalmap.org/",
-	          authURL: "https://www.openhistoricalmap.org/",
+	OsmServer(fullName: "OpenHistoricalMap",
+	          apiHost: "www.openhistoricalmap.org",
+	          authHost: "www.openhistoricalmap.org",
+	          nominatimHost: "nominatim-api.openhistoricalmap.org",
 	          client_id: "UnjOHLFzRNc1VKeQJYz2ptqJu_K8fj2hVSsGkTLFjC4"),
 
-	OsmServer(ident: .ogf,
-	          fullName: "OpenGeoFiction",
-	          apiURL: "https://opengeofiction.net/",
-	          authURL: "https://opengeofiction.net/",
+	OsmServer(fullName: "OpenGeoFiction",
+	          apiHost: "opengeofiction.net",
+	          authHost: "opengeofiction.net",
+	          nominatimHost: "nominatim.opengeofiction.net",
 	          client_id: "EwlSaN_GLSz6fhkncxU4GWtXG1NyEy2z63QER9ISGFA")
 ]

src/Shared/MapMarkers/MapMarkerDatabase.swift
@@ -222,7 +222,7 @@ final class MapMarkerDatabase: MapMarkerIgnoreListProtocol {
 
 extension MapMarkerDatabase {
 	func addNoteMarkers(forRegion box: OSMRect, completion: @escaping () -> Void) {
-		let url = OSM_API_URL +
+		let url = OSM_SERVER.apiURL +
 			"api/0.6/notes?closed=0&bbox=\(box.origin.x),\(box.origin.y),\(box.origin.x + box.size.width),\(box.origin.y + box.size.height)"
 		if let url1 = URL(string: url) {
 			URLSession.shared.data(with: url1, completionHandler: { [self] result in
@@ -263,7 +263,7 @@ extension MapMarkerDatabase {
 		allowedChars.remove(charactersIn: "+;&")
 		let comment = comment.addingPercentEncoding(withAllowedCharacters: allowedChars) ?? ""
 
-		var url = OSM_API_URL + "api/0.6/notes"
+		var url = OSM_SERVER.apiURL + "api/0.6/notes"
 
 		if note.comments.count == 0 {
 			// brand new note

src/Shared/PresetsDatabase/TagInfo.swift
@@ -58,7 +58,7 @@ class TagInfo {
 	class func taginfoFor(key: String, searchKeys: Bool, update: @escaping ([String]) -> Void) {
 		DispatchQueue.global(qos: .default).async(execute: {
 			let cleanKey = searchKeys ? key.trimmingCharacters(in: CharacterSet(charactersIn: ":")) : key
-			let abibase = "https://taginfo.openstreetmap.org/api/4"
+			let abibase = "\(OSM_SERVER.taginfoUrl)api/4"
 			let urlText = searchKeys
 				? "\(abibase)/keys/all?query=\(cleanKey)&page=1&rp=25&sortname=count_all&sortorder=desc"
 				: "\(abibase)/key/values?key=\(cleanKey)&page=1&rp=25&sortname=count_all&sortorder=desc"
@@ -127,7 +127,7 @@ class TagInfo {
 	// search the taginfo database
 	class func wikiInfoFor(key: String, value: String, update: @escaping (String) -> Void) {
 		DispatchQueue.global(qos: .default).async(execute: {
-			let abibase = "https://taginfo.openstreetmap.org/api/4/"
+			let abibase = "\(OSM_SERVER.taginfoUrl)api/4/"
 			let urlText: String
 			if value == "" {
 				urlText = "\(abibase)key/wiki_pages?key=\(key)"

src/Shared/Utility/Debugger.swift
@@ -29,7 +29,7 @@ func DLog(_ args: String...) {
 
 func DbgAssert(_ x: Bool) {
 #if DEBUG
-	isUnderDebugger() {
+	if isUnderDebugger() {
 		assert(x, "unspecified")
 	}
 #endif

src/Shared/Utility/OAuth2.swift
@@ -35,7 +35,7 @@ class OAuth2 {
 	private(set) var authorizationHeader: (name: String, value: String)?
 
 	// These values are all computed dynamically based on the current server (kinda ugly)
-	var server: OsmServer { OsmServerList.first(where: { $0.apiURL == OSM_API_URL }) ?? OsmServerList[0] }
+	var server: OsmServer { OSM_SERVER }
 	var client_id: String { server.client_id }
 	var serverURL: String { server.authURL }
 	var oauthUrl: URL { return URL(string: serverURL)!.appendingPathComponent("oauth2") }

src/iOS/DisplaySettings/GpxViewController.swift
@@ -154,7 +154,7 @@ class GpxViewController: UITableViewController {
 
 		// let progress window display before we submit work
 		DispatchQueue.main.async(execute: {
-			let url = OSM_API_URL + "api/0.6/gpx/create"
+			let url = OSM_SERVER.apiURL + "api/0.6/gpx/create"
 
 			guard var request = AppDelegate.shared.oAuth2.urlRequest(string: url) else { return }
 			let boundary = "----------------------------d10f7aa230e8"

src/iOS/GoMapTests/OSMMapDataTestCase.swift
@@ -25,16 +25,12 @@ class OSMMapDataTestCase: XCTestCase {
 
 	func testSetServerShouldAddThePathSeparatorSuffixIfItDoesNotExist() {
 		let hostname = "https://example.com"
-		mapData.setServer(hostname)
-
 		let hostnameWithPathSeparatorSuffix = "\(hostname)/"
-		XCTAssertEqual(OSM_API_URL, hostnameWithPathSeparatorSuffix)
+		XCTAssertEqual(OsmServer.serverForUrl(hostname).apiURL, hostnameWithPathSeparatorSuffix)
 	}
 
 	func testSetServerShouldNotAddThePathSeparatorSuffixIfItAlreadyExists() {
 		let hostname = "https://example.com/"
-		mapData.setServer(hostname)
-
-		XCTAssertEqual(OSM_API_URL, hostname)
+		XCTAssertEqual(OsmServer.serverForUrl(hostname).apiURL, hostname)
 	}
 }

commit 6ee665c6c3e2792730d36243b133c7f2fe704712

Fix a bug introduced by 7aac560e5fde4a3f2c8d182787617280a463698c that deleted the Sqlite database at startup

src/Shared/OSMModels/OsmMapData.swift
@@ -72,7 +72,7 @@ final class OsmMapData: NSObject, NSSecureCoding {
 
 	// MARK: Utility
 
-	func setServer(_ host: OsmServer) {
+	func resetServer(_ host: OsmServer) {
 		if OSM_SERVER.apiURL.count != 0 {
 			// get rid of old data before connecting to new server
 			purgeSoft()
@@ -1167,7 +1167,6 @@ final class OsmMapData: NSObject, NSSecureCoding {
 	// MARK: Init/Save/Restore
 
 	func initCommon() {
-		setServer(OSM_SERVER)
 		setupPeriodicSaveTimer()
 	}
 
@@ -1350,8 +1349,8 @@ final class OsmMapData: NSObject, NSSecureCoding {
 		deleteRelations: [OsmRelation],
 		isUpdate: Bool)
 	{
-		if (saveNodes.count + saveWays.count + saveRelations.count + deleteNodes.count + deleteWays
-			.count + deleteRelations.count) == 0
+		if (saveNodes.count + saveWays.count + saveRelations.count +
+			deleteNodes.count + deleteWays.count + deleteRelations.count) == 0
 		{
 			return
 		}

src/Shared/OSMModels/OsmServer.swift
@@ -20,7 +20,7 @@ var OSM_SERVER: OsmServer = {
 			return
 		}
 		UserPrefs.shared.osmServerUrl.value = OSM_SERVER.apiURL
-		AppDelegate.shared.mapView.editorLayer.mapData.setServer(OSM_SERVER)
+		AppDelegate.shared.mapView.editorLayer.mapData.resetServer(OSM_SERVER)
 	}
 }
 