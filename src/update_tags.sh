a0faa901f495d736b7fb8d8739435f1b0c7731bc
Bryce Cogswell <bryceco@yahoo.com>
2024-06-26 14:40:10 -0700

    Update script to tag versions

--- src/update_tags.sh
+++ src/update_tags.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
+
 # Tag all commits with version changes in the project
+
+if [ -z "$(git status --porcelain)" ]; then
+	echo ""
+else
+    echo "Commit pending changes and try again"
+    exit 1
+fi
 
 git log -p -- 'iOS/Go Map!!.xcodeproj/project.pbxproj' |
 egrep '(^commit|([+].*MARKETING))' |
@@ -6,3 +15,4 @@ sed 's/;//' |
 awk '/commit/ { commit = $2 } /MARKETING/ { print "git tag", $4, commit }' |
 uniq |
 sh
+git push --tags
