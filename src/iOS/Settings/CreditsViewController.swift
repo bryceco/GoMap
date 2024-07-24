b2774554d3504892d7235f56af5e2f1b939e31bf
Bryce Cogswell <bryceco@yahoo.com>
2024-07-23 20:22:55 -0700

    Update some copyright dates. Automatically convert displayed copyright date to the current year.

--- src/iOS/Settings/CreditsViewController.swift
+++ src/iOS/Settings/CreditsViewController.swift
@@ -15,6 +15,12 @@ class CreditsViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		let regex = "2012-20[0-9][0-9]"
+		if let range = textView.text.range(of: regex, options: [.regularExpression]) {
+			let newYear = Calendar(identifier: .gregorian).component(.year, from: Date())
+			let newRange = "2012-\(newYear)"
+			textView.text = textView.text.replacingCharacters(in: range, with: newRange)
+		}
 		textView.isEditable = false
 		textView.layer.cornerRadius = 10.0
 	}
