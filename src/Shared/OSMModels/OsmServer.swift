b9bf6b1ab8b3acd01b01b47573385a07dfeee083
Bryce Cogswell <bryceco@yahoo.com>
2024-07-28 14:53:55 -0700

    Add OsmCha server to OsmServer struct

--- src/Shared/OSMModels/OsmServer.swift
+++ src/Shared/OSMModels/OsmServer.swift
@@ -29,6 +29,7 @@ struct OsmServer {
 	let apiURL: String // used for API connections
 	let authURL: String // used for OAuth connections
 	let nominatimUrl: String
+	let osmchaUrl: String?
 	let client_id: String // used for OAuth connections
 
 	var queryURL: String { // used for things like "www.openstreetmap.org/user/bryceco"
@@ -39,12 +40,18 @@ struct OsmServer {
 	     apiHost: String,
 	     authHost: String,
 	     nominatimHost: String,
+		 osmchaHost: String?,
 	     client_id: String)
 	{
 		self.fullName = fullName
 		apiURL = "https://" + apiHost + "/"
 		authURL = "https://" + authHost + "/"
 		nominatimUrl = "https://" + nominatimHost + "/"
+		if let osmchaHost = osmchaHost {
+			osmchaUrl = "https://" + osmchaHost + "/"
+		} else {
+			osmchaUrl = nil
+		}
 		self.client_id = client_id
 	}
 
@@ -53,6 +60,7 @@ struct OsmServer {
 		apiURL = apiUrl
 		authURL = ""
 		nominatimUrl = ""
+		osmchaUrl = nil
 		client_id = ""
 	}
 
@@ -104,23 +112,27 @@ let OsmServerList = [
 	          apiHost: "api.openstreetmap.org",
 	          authHost: "www.openstreetmap.org",
 	          nominatimHost: "nominatim.openstreetmap.org",
+			  osmchaHost: "osmcha.org",
 	          client_id: "mzRn5Z-X0nSptHgA3o5g30HeaaljTXfv0GMOLhmwqeo"),
 
 	OsmServer(fullName: "OSM Dev Server",
 	          apiHost: "api06.dev.openstreetmap.org",
 	          authHost: "master.apis.dev.openstreetmap.org",
 	          nominatimHost: "nominatim.openstreetmap.org",
+			  osmchaHost: nil,
 	          client_id: "SGsePsBukg7xPkIaqNIQlAgiAa3vjauIFkbcsPXB2Tg"),
 
 	OsmServer(fullName: "OpenHistoricalMap",
 	          apiHost: "www.openhistoricalmap.org",
 	          authHost: "www.openhistoricalmap.org",
 	          nominatimHost: "nominatim-api.openhistoricalmap.org",
+			  osmchaHost: "osmcha.openhistoricalmap.org",
 	          client_id: "UnjOHLFzRNc1VKeQJYz2ptqJu_K8fj2hVSsGkTLFjC4"),
 
 	OsmServer(fullName: "OpenGeoFiction",
 	          apiHost: "opengeofiction.net",
 	          authHost: "opengeofiction.net",
 	          nominatimHost: "nominatim.opengeofiction.net",
+			  osmchaHost: nil,
 	          client_id: "EwlSaN_GLSz6fhkncxU4GWtXG1NyEy2z63QER9ISGFA")
 ]
