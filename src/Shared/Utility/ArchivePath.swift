72e646982ed19ade56f9e89222996cc9f51dcad6
Bryce Cogswell <bryceco@yahoo.com>
2025-06-16 13:45:30 -0700

    Update ArchivePath to store more data in the Documents folder

--- src/Shared/Utility/ArchivePath.swift
+++ src/Shared/Utility/ArchivePath.swift
@@ -10,72 +10,27 @@ import Foundation
 
 // Provides a centralized place to keep track of locations of various resources.
 enum ArchivePath {
-	case aerialProviers
-	case customPresets // deprecated, these are stored in UserDefaults now
+	// folders
+	case appDataFolder
 	case geoJSONs
 	case gpxPoints
+	case webCache(String)
+
+	// files
+	case aerialProviers
+	case legacyCustomPresets // deprecated, these are stored in UserDefaults now
 	case mapMarkerIgnoreList
 	case osmDataArchive
 	case sqlite(String)
 	case tagInfo
-	case webCache(String)
 
 	func path() -> String {
 		return url().path
 	}
 
-	func url() -> URL {
-		switch self {
-		case .aerialProviers:
-			return Self.urlFor(file: "OSM Aerial Providers.json",
-			                   in: .libraryDirectory,
-			                   bundleID: true,
-			                   upgrading: [.cachesDirectory])
-		case .customPresets:
-			return Self.urlFor(file: "CustomPresetList.data",
-			                   in: .libraryDirectory,
-			                   bundleID: false)
-		case .geoJSONs:
-			return Self.urlFor(folder: "geoJSON",
-			                   in: .documentDirectory,
-			                   bundleID: false)
-		case .gpxPoints:
-			return Self.urlFor(folder: "gpxPoints",
-			                   in: .documentDirectory,
-			                   bundleID: false)
-		case .mapMarkerIgnoreList:
-			return Self.urlFor(file: "mapMarkerIgnoreList",
-			                   in: .libraryDirectory,
-			                   bundleID: false)
-		case .osmDataArchive:
-			return Self.urlFor(file: "OSM Downloaded Data.archive",
-			                   in: .libraryDirectory,
-			                   bundleID: true,
-			                   upgrading: [.cachesDirectory])
-		case let .sqlite(name):
-			assert(name != "")
-			return Self.urlFor(file: name,
-			                   in: .libraryDirectory,
-			                   bundleID: true,
-			                   upgrading: [.cachesDirectory])
-		case .tagInfo:
-			return Self.urlFor(file: "tagInfo.plist",
-			                   in: .libraryDirectory,
-			                   bundleID: true)
-		case let .webCache(name):
-			assert(name != "")
-			return Self.urlFor(folder: name,
-			                   in: .cachesDirectory,
-			                   bundleID: true)
-		}
-	}
-}
-
-private extension ArchivePath {
-	private static func urlFor(name: String,
-	                           isFolder: Bool,
-	                           in folder: FileManager.SearchPathDirectory,
-	                           bundleID: Bool) -> URL
+	func urlWith(name: String,
+	             in folder: FileManager.SearchPathDirectory,
+	             bundleID: Bool = false) -> URL
 	{
 		var url = try! FileManager.default.url(for: folder,
 		                                       in: .userDomainMask,
@@ -86,63 +41,108 @@ private extension ArchivePath {
 			url = url.appendingPathComponent(bundleName, isDirectory: true)
 		}
 		url = url.appendingPathComponent(name, isDirectory: false)
-		if isFolder {
-			do {
-				try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
-			} catch {
-				print("\(error)")
-			}
-		}
 		return url
 	}
 
-	private static func urlFor(file name: String,
-	                           in folder: FileManager.SearchPathDirectory,
-	                           bundleID: Bool) -> URL
+	func urlWith(name: String,
+	             in folder: URL,
+	             legacy: [URL]) -> URL
 	{
-		return Self.urlFor(name: name,
-		                   isFolder: false,
-		                   in: folder,
-		                   bundleID: bundleID)
-	}
-
-	private static func urlFor(folder name: String,
-	                           in folder: FileManager.SearchPathDirectory,
-	                           bundleID: Bool) -> URL
-	{
-		return Self.urlFor(name: name,
-		                   isFolder: true,
-		                   in: folder,
-		                   bundleID: bundleID)
-	}
-
-	// Compute the path to a file given. If the file only exists at a legacy location
-	// them move the file to the new, correct location.
-	private static func urlFor(file name: String,
-	                           in preferred: FileManager.SearchPathDirectory,
-	                           bundleID: Bool,
-	                           upgrading legacyDirs: [FileManager.SearchPathDirectory]) -> URL
-	{
-		let preferredURL = urlFor(file: name, in: preferred, bundleID: bundleID)
+		let preferredURL = folder.appendingPathComponent(name, isDirectory: false)
 		if FileManager.default.fileExists(atPath: preferredURL.path) {
 			return preferredURL
 		}
 		try? FileManager.default.createDirectory(at: preferredURL.deletingLastPathComponent(),
 		                                         withIntermediateDirectories: true)
-		for legacyDir in legacyDirs {
+		for legacyURL in legacy {
 			// If the file is at a secondary location then move it to the preferred location.
-			let url = urlFor(file: name, in: legacyDir, bundleID: bundleID)
 			do {
-				try FileManager.default.moveItem(at: url, to: preferredURL)
+				try FileManager.default.moveItem(at: legacyURL, to: preferredURL)
 			} catch {
 				if (error as NSError).domain == NSCocoaErrorDomain,
 				   (error as NSError).code == NSFileWriteFileExistsError
 				{
 					// already have a copy, so delete this one
-					try? FileManager.default.removeItem(at: url)
+					try? FileManager.default.removeItem(at: legacyURL)
 				}
 			}
 		}
 		return preferredURL
+	}
+
+	func folderURL(_ url: URL) -> URL {
+		try? FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+		return url
+	}
+
+	func url() -> URL {
+		switch self {
+
+		case .appDataFolder:
+			// a folder under Documents that contains edits and downloaded OSM data
+			return folderURL(urlWith(name: "appData", in: .documentDirectory))
+
+		case .geoJSONs:
+			return folderURL(urlWith(name: "geoJSON", in: .documentDirectory))
+
+		case .gpxPoints:
+			return folderURL(urlWith(name: "gpxPoints", in: .documentDirectory))
+
+		case let .webCache(name):
+			assert(name != "")
+			return folderURL(urlWith(name: name,
+			                         in: .cachesDirectory,
+			                         bundleID: true))
+
+			//
+			// Folder paths are above here, file paths are below
+			//
+
+		case .aerialProviers:
+			return urlWith(name: "Aerial Providers.json",
+			               in: Self.appDataFolder.url(),
+			               legacy: [
+			               	urlWith(name: "OSM Aerial Providers.json", in: .libraryDirectory, bundleID: true),
+			               	urlWith(name: "OSM Aerial Providers.json", in: .cachesDirectory, bundleID: true)
+			               ])
+
+		case .legacyCustomPresets:
+			return urlWith(name: "CustomPresetList.data",
+			               in: Self.appDataFolder.url(),
+			               legacy: [
+			               	urlWith(name: "CustomPresetList.data", in: .libraryDirectory)
+			               ])
+
+		case .mapMarkerIgnoreList:
+			return urlWith(name: "mapMarkerIgnoreList.archive",
+			               in: Self.appDataFolder.url(),
+			               legacy: [
+			               	urlWith(name: "mapMarkerIgnoreList", in: .libraryDirectory)
+			               ])
+
+		case .osmDataArchive:
+			return urlWith(name: "edits.archive",
+			               in: Self.appDataFolder.url(),
+			               legacy: [
+			               	urlWith(name: "OSM Downloaded Data.archive", in: .libraryDirectory, bundleID: true),
+			               	urlWith(name: "OSM Downloaded Data.archive", in: .cachesDirectory, bundleID: true)
+			               ])
+
+		case let .sqlite(name):
+			assert(name != "")
+			return urlWith(name: name,
+			               in: Self.appDataFolder.url(),
+			               legacy: [
+			               	urlWith(name: name, in: .libraryDirectory, bundleID: true),
+			               	urlWith(name: name, in: .cachesDirectory, bundleID: true)
+			               ])
+
+		case .tagInfo:
+			return urlWith(name: "tagInfo.plist",
+			               in: Self.appDataFolder.url(),
+			               legacy: [
+			               	urlWith(name: "tagInfo.plist", in: .libraryDirectory, bundleID: true)
+			               ])
+		}
 	}
 }
