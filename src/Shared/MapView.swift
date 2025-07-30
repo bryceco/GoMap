bryceco/GoMap

commit 7e72aa28c132259427116afc58e7f782b4f04213

Rework panoramax camera code to include orientation

src/Shared/MapView.swift
@@ -32,9 +32,9 @@ struct MapViewOverlays: OptionSet {
 }
 
 enum GPS_STATE: Int {
-	case NONE
-	case LOCATION
-	case HEADING
+	case NONE // none
+	case LOCATION // location only
+	case HEADING // location and heading
 }
 
 enum EDIT_ACTION: Int {
@@ -1695,6 +1695,8 @@ final class MapView: UIView, MapViewProgress, CLLocationManagerDelegate, UIActio
 
 	private var locationManagerSmoothHeading = 0.0
 	func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+		guard gpsState == .HEADING else { return }
+
 		let accuracy = newHeading.headingAccuracy
 		let heading = self.heading(for: newHeading)
 
@@ -1777,6 +1779,7 @@ final class MapView: UIView, MapViewProgress, CLLocationManagerDelegate, UIActio
 	}
 
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		guard gpsState != .NONE else { return }
 		for location in locations {
 			locationUpdated(to: location)
 		}

src/iOS/Panoramax.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2025 Bryce Cogswell. All rights reserved.
 //
 
-import Foundation
 import UIKit
 @preconcurrency import WebKit
 
@@ -174,7 +173,6 @@ class PanoramaxServer {
 	func uploadTo(photoSet: String,
 	              photoData: Data,
 	              name: String,
-	              location: LatLon,
 	              date: Date,
 	              callback: @escaping @MainActor(Result<String, Error>) -> Void)
 	{
@@ -196,16 +194,6 @@ class PanoramaxServer {
 		body.append("Content-Type: application/octet-stream\r\n\r\n".data(using: .utf8)!)
 		body.append(photoData)
 		body.append("\r\n".data(using: .utf8)!)
-
-		// Add latitude
-		body.append("--\(boundary)\r\n".data(using: .utf8)!)
-		body.append("Content-Disposition: form-data; name=\"override_latitude\"\r\n\r\n".data(using: .utf8)!)
-		body.append("\(location.lat)\r\n".data(using: .utf8)!)
-
-		// Add longitude
-		body.append("--\(boundary)\r\n".data(using: .utf8)!)
-		body.append("Content-Disposition: form-data; name=\"override_longitude\"\r\n\r\n".data(using: .utf8)!)
-		body.append("\(location.lon)\r\n".data(using: .utf8)!)
 
 		// Add capture time
 		body.append("--\(boundary)\r\n".data(using: .utf8)!)
@@ -291,6 +279,27 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 		if !photoID.isEmpty {
 			fetchPhotoAndMetadata()
 		}
+
+		// start location services in case user takes a photo
+		AppDelegate.shared.mapView.locationManager.startUpdatingLocation()
+		AppDelegate.shared.mapView.locationManager.startUpdatingHeading()
+	}
+
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+
+		// return location services to expected state
+		if isBeingDismissed || navigationController?.isBeingDismissed == true {
+			switch AppDelegate.shared.mapView.gpsState {
+			case .NONE:
+				AppDelegate.shared.mapView.locationManager.stopUpdatingLocation()
+				AppDelegate.shared.mapView.locationManager.stopUpdatingHeading()
+			case .LOCATION:
+				AppDelegate.shared.mapView.locationManager.stopUpdatingHeading()
+			case .HEADING:
+				break
+			}
+		}
 	}
 
 	@IBAction
@@ -309,18 +318,24 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 		present(webVC, animated: true)
 	}
 
+	var photoPicker: PhotoCapture!
+
 	@IBAction
 	func captureAndUploadPhotograph(_ sender: Any) {
 		let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
 		alert.addAction(UIAlertAction(
 			title: NSLocalizedString("Take New Photo", comment: ""),
 			style: .default,
-			handler: { _ in
-				let vc = UIImagePickerController()
-				vc.sourceType = .camera
-				vc.allowsEditing = false
-				vc.delegate = self
-				self.present(vc, animated: true)
+			handler: { [self] _ in
+				photoPicker = PhotoCapture()
+				photoPicker.locationManager = AppDelegate.shared.mapView.locationManager
+				photoPicker.onCancel = {}
+				photoPicker.onError = {}
+				photoPicker.onAccept = { image, imageData in
+					self.uploadImage(image: image, imageData: imageData)
+				}
+				photoPicker.modalPresentationStyle = .fullScreen
+				self.present(photoPicker, animated: true)
 			}))
 		alert.addAction(UIAlertAction(
 			title: NSLocalizedString("Choose Existing Photo", comment: ""),
@@ -355,20 +370,16 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 	{
 		picker.dismiss(animated: true)
 
-		// get image
-		guard let image = info[.originalImage] as? UIImage
+		guard let image = info[.originalImage] as? UIImage,
+		      let imageURL = info[.imageURL] as? URL,
+		      let imageData = try? Data(contentsOf: imageURL)
 		else {
-			showError(NSError(domain: "Bad image data", code: 1))
 			return
 		}
-		uploadImage(image)
+		uploadImage(image: image, imageData: imageData)
 	}
 
-	func uploadImage(_ image: UIImage) {
-		guard let data = image.jpegData(compressionQuality: 0.9) else {
-			return
-		}
-
+	func uploadImage(image: UIImage, imageData: Data) {
 		// get date and name
 		let date = Date()
 		let formatter = DateFormatter()
@@ -405,7 +416,7 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 							// try again
 							// Without the pause the server doesn't always accept our cookie
 							DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-								self.uploadImage(image)
+								self.uploadImage(image: image, imageData: imageData)
 							}
 						case .failure:
 							break
@@ -417,9 +428,8 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 				return
 			case let .success(photoSetID):
 				self.panoramax.uploadTo(photoSet: photoSetID,
-				                        photoData: data,
+				                        photoData: imageData,
 				                        name: name,
-				                        location: self.location,
 				                        date: date,
 				                        callback: { result in
 				                        	switch result {

src/iOS/Go Map!!.xcodeproj/project.pbxproj
@@ -145,6 +145,7 @@
 		02CBF3CD2BE86646000DE2AD /* GpxTrackAttributes.swift in Sources */ = {isa = PBXBuildFile; fileRef = 02CBF3CB2BE8655D000DE2AD /* GpxTrackAttributes.swift */; };
 		02CC01D12584195E004444EF /* PresetFeature.swift in Sources */ = {isa = PBXBuildFile; fileRef = 02CC01D02584195E004444EF /* PresetFeature.swift */; };
 		02CC01DC25843F2F004444EF /* PresetLanguages.swift in Sources */ = {isa = PBXBuildFile; fileRef = 02CC01DB25843F2F004444EF /* PresetLanguages.swift */; };
+		02CC7E3D2E2EF248001CA356 /* PhotoCapture.swift in Sources */ = {isa = PBXBuildFile; fileRef = 02CC7E3C2E2EF248001CA356 /* PhotoCapture.swift */; };
 		02CD9974265C65C200213AD6 /* EditorFilters.swift in Sources */ = {isa = PBXBuildFile; fileRef = 02CD9973265C65C200213AD6 /* EditorFilters.swift */; };
 		02CE3CE229909D1D00DDACE0 /* QuestSolverController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 02CB523E299097C3007F758D /* QuestSolverController.swift */; };
 		02CE3CE32990A2C800DDACE0 /* QuestSolver.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = 02CB523D299097C3007F758D /* QuestSolver.storyboard */; };
@@ -479,6 +480,7 @@
 		02CBF3CB2BE8655D000DE2AD /* GpxTrackAttributes.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = GpxTrackAttributes.swift; sourceTree = "<group>"; };
 		02CC01D02584195E004444EF /* PresetFeature.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PresetFeature.swift; sourceTree = "<group>"; };
 		02CC01DB25843F2F004444EF /* PresetLanguages.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PresetLanguages.swift; sourceTree = "<group>"; };
+		02CC7E3C2E2EF248001CA356 /* PhotoCapture.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PhotoCapture.swift; sourceTree = "<group>"; };
 		02CD9973265C65C200213AD6 /* EditorFilters.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EditorFilters.swift; sourceTree = "<group>"; };
 		02CE3CE42990A8CE00DDACE0 /* QuestChooserController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = QuestChooserController.swift; sourceTree = "<group>"; };
 		02CE3CE6299187C100DDACE0 /* TelephoneToolbar.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = TelephoneToolbar.swift; sourceTree = "<group>"; };
@@ -858,6 +860,7 @@
 				021BC2212D7625FB004631C5 /* Panoramax.storyboard */,
 				021BC21D2D6E6E05004631C5 /* Panoramax.swift */,
 				C3D0969F263CD4BD00E3B129 /* PersistentWebCache.swift */,
+				02CC7E3C2E2EF248001CA356 /* PhotoCapture.swift */,
 				02CC01CF2584190A004444EF /* PresetsDatabase */,
 				C365A568263FCC9C007DEC94 /* TapAndDragGesture.swift */,
 				02F5329A267AD0250088DF41 /* Tiles */,
@@ -1637,6 +1640,7 @@
 				02CB2406265DB7CB00835F32 /* CameraView.swift in Sources */,
 				CD677FFA2A5FC17300F28605 /* AccountInfoTableViewController.swift in Sources */,
 				6442666722540EDF00C0D545 /* Lock.swift in Sources */,
+				02CC7E3D2E2EF248001CA356 /* PhotoCapture.swift in Sources */,
 				02C8C56726F44728003B0122 /* OsmNoteMarker.swift in Sources */,
 				02505C5B29A52EEE0078D60D /* QuestDefinitionWithFeatures.swift in Sources */,
 				028816A12659B4A600C30EA2 /* GZip.swift in Sources */,
@@ -1968,6 +1972,7 @@
 				SWIFT_ENABLE_BATCH_MODE = NO;
 				SWIFT_OBJC_BRIDGING_HEADER = "$(SRCROOT)/Go Map!!-Bridging-Header.h";
 				SWIFT_OPTIMIZATION_LEVEL = "-Onone";
+				SWIFT_PRECOMPILE_BRIDGING_HEADER = NO;
 				SWIFT_VERSION = 5.0;
 				TARGETED_DEVICE_FAMILY = "1,2,6";
 				VERSIONING_SYSTEM = "apple-generic";
@@ -2012,6 +2017,7 @@
 				SWIFT_EMIT_LOC_STRINGS = YES;
 				SWIFT_ENABLE_BATCH_MODE = NO;
 				SWIFT_OBJC_BRIDGING_HEADER = "$(SRCROOT)/Go Map!!-Bridging-Header.h";
+				SWIFT_PRECOMPILE_BRIDGING_HEADER = NO;
 				SWIFT_VERSION = 5.0;
 				TARGETED_DEVICE_FAMILY = "1,2,6";
 				VERSIONING_SYSTEM = "apple-generic";

src/iOS/POI/PresetValueTextField.swift
@@ -507,18 +507,6 @@ class PresetValueTextField: AutocompleteTextField, PanororamaxDelegate {
 	@objc func openPanoramaxViewController(_ sender: Any?) {
 		resignFirstResponder()
 
-		// get location
-		let mapView = AppDelegate.shared.mapView!
-		let location: LatLon
-		if let object = mapView.editorLayer.selectedPrimary?.selectionPoint() {
-			location = object
-		} else if let pushPin = mapView.pushPin?.arrowPoint {
-			location = mapView.mapTransform.latLon(forScreenPoint: pushPin)
-		} else {
-			// shouldn't get here
-			return
-		}
-
 		// save our key value so we can locate the correct cell when we come back
 		panoramaxKey = key
 
@@ -526,7 +514,6 @@ class PresetValueTextField: AutocompleteTextField, PanororamaxDelegate {
 		vc.panoramax = PanoramaxServer(serverURL: URL(string: "https://panoramax.openstreetmap.fr")!)
 		vc.photoID = text ?? ""
 		vc.delegate = self
-		vc.location = location
 		owner.viewController?.present(vc, animated: true)
 	}
 }

src/iOS/PhotoCapture.swift
@@ -1,0 +1,298 @@
+//
+//  PhotoCapture.swift
+//  Go Map!!
+//
+//  Created by Bryce Cogswell on 7/21/25.
+//  Copyright © 2025 Bryce Cogswell. All rights reserved.
+//
+
+import CoreLocation
+import Foundation
+import UIKit
+
+private func dataFor(image: UIImage, location: CLLocation?, heading: CLHeading?) -> Data? {
+	guard let imageData = image.jpegData(compressionQuality: 0.9),
+	      let source = CGImageSourceCreateWithData(imageData as CFData, nil),
+	      let imageType = CGImageSourceGetType(source),
+	      let metadataOrig = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) as? [String: Any]
+	else {
+		return nil
+	}
+
+	var gpsDict: [String: Any] = [:]
+
+	// Inject location values
+	if let loc = location {
+		let timeFormatter = DateFormatter()
+		timeFormatter.timeZone = TimeZone(abbreviation: "UTC")
+		timeFormatter.dateFormat = "HH:mm:ss"
+
+		let dateFormatter = DateFormatter()
+		dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+		dateFormatter.dateFormat = "yyyy:MM:dd"
+
+		gpsDict[kCGImagePropertyGPSLatitude as String] = abs(loc.coordinate.latitude)
+		gpsDict[kCGImagePropertyGPSLatitudeRef as String] = loc.coordinate.latitude >= 0 ? "N" : "S"
+		gpsDict[kCGImagePropertyGPSLongitude as String] = abs(loc.coordinate.longitude)
+		gpsDict[kCGImagePropertyGPSLongitudeRef as String] = loc.coordinate.longitude >= 0 ? "E" : "W"
+		gpsDict[kCGImagePropertyGPSAltitude as String] = loc.altitude
+		gpsDict[kCGImagePropertyGPSAltitudeRef as String] = loc.altitude >= 0 ? 0 : 1
+		gpsDict[kCGImagePropertyGPSDateStamp as String] = dateFormatter.string(from: loc.timestamp)
+		gpsDict[kCGImagePropertyGPSTimeStamp as String] = timeFormatter.string(from: loc.timestamp)
+	}
+
+	// Inject heading values
+	if let hdg = heading {
+		gpsDict[kCGImagePropertyGPSImgDirection as String] = hdg.trueHeading
+		gpsDict[kCGImagePropertyGPSImgDirectionRef as String] = "T" // "T" for true north
+	}
+
+	var metadata = metadataOrig
+	metadata[kCGImagePropertyGPSDictionary as String] = gpsDict
+
+	// Write the new data to exif
+	let outputData = NSMutableData()
+	guard let destination = CGImageDestinationCreateWithData(outputData, imageType, 1, nil) else {
+		return nil
+	}
+	CGImageDestinationAddImageFromSource(destination, source, 0, metadata as CFDictionary)
+	CGImageDestinationFinalize(destination)
+
+	return outputData as Data
+}
+
+private class CameraOverlayView: UIView {
+	enum State {
+		case capturing
+		case reviewing
+	}
+
+	let cancelButton = UIButton(type: .system)
+	let shutterButton = makeShutterButton(size: 50)
+	let retakeButton = UIButton(type: .system)
+	let acceptButton = UIButton(type: .system)
+
+	private let previewView = UIImageView()
+	private var currentState: State = .capturing
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		backgroundColor = .clear
+		setupButtons()
+		switchToState(.capturing)
+	}
+
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	private static func makeShutterButton(size: CGFloat) -> UIButton {
+		let button = UIButton()
+		button.backgroundColor = .white
+
+		button.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			button.widthAnchor.constraint(equalToConstant: size),
+			button.heightAnchor.constraint(equalTo: button.widthAnchor)
+		])
+
+		// Make circular
+		button.layer.cornerRadius = size / 2
+		button.layer.masksToBounds = true
+
+		// Add black ring just inside edge
+		let ringLayer = CAShapeLayer()
+		let inset: CGFloat = 2.0
+		let radius = (size / 2) - inset
+		let ringPath = UIBezierPath(arcCenter: CGPoint(x: size / 2, y: size / 2),
+		                            radius: radius,
+		                            startAngle: 0,
+		                            endAngle: .pi * 2,
+		                            clockwise: true)
+		ringLayer.path = ringPath.cgPath
+		ringLayer.strokeColor = UIColor.black.cgColor
+		ringLayer.fillColor = UIColor.clear.cgColor
+		ringLayer.lineWidth = 1.0
+		button.layer.addSublayer(ringLayer)
+		return button
+	}
+
+	private func setupButtons() {
+		if #available(iOS 13.0, *) {
+			cancelButton.setImage(UIImage(systemName: "xmark.circle"), for: .normal)
+			acceptButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+			retakeButton.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+		} else {
+			cancelButton.setTitle("Cancel", for: .normal)
+			acceptButton.setTitle("Use Photo", for: .normal)
+			retakeButton.setTitle("Retake", for: .normal)
+		}
+		[shutterButton, cancelButton, retakeButton, acceptButton].forEach { button in
+			button.translatesAutoresizingMaskIntoConstraints = false
+			addSubview(button)
+		}
+
+		NSLayoutConstraint.activate([
+			cancelButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+			cancelButton.centerYAnchor.constraint(equalTo: shutterButton.centerYAnchor),
+
+			shutterButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+			shutterButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+
+			acceptButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+			acceptButton.centerYAnchor.constraint(equalTo: shutterButton.centerYAnchor),
+
+			retakeButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+			retakeButton.centerYAnchor.constraint(equalTo: shutterButton.centerYAnchor)
+		])
+
+		// Preview after image is captured
+		previewView.contentMode = .scaleAspectFit
+		previewView.isHidden = true
+		addSubview(previewView)
+		previewView.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			previewView.leadingAnchor.constraint(equalTo: leadingAnchor),
+			previewView.trailingAnchor.constraint(equalTo: trailingAnchor),
+			previewView.topAnchor.constraint(equalTo: topAnchor),
+			previewView.bottomAnchor.constraint(equalTo: bottomAnchor)
+		])
+	}
+
+	func switchToState(_ state: State) {
+		currentState = state
+
+		switch state {
+		case .capturing:
+			shutterButton.isHidden = false
+			retakeButton.isHidden = true
+			acceptButton.isHidden = true
+		case .reviewing:
+			shutterButton.isHidden = true
+			retakeButton.isHidden = false
+			acceptButton.isHidden = false
+		}
+	}
+
+	func showPreview(_ image: UIImage) {
+		previewView.image = image
+		previewView.alpha = 0
+		previewView.isHidden = false
+		UIView.animate(withDuration: 0.3) {
+			self.previewView.alpha = 1
+		}
+	}
+
+	func hidePreview() {
+		previewView.isHidden = true
+		previewView.image = nil
+	}
+}
+
+class PhotoCapture: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	private let picker = UIImagePickerController()
+	private let overlayView = CameraOverlayView()
+	private var capturedImage: UIImage?
+	private var capturedImageData: Data?
+
+	var locationManager: CLLocationManager?
+	var onAccept: ((UIImage, Data) -> Void)?
+	var onCancel: (() -> Void)?
+	var onError: (() -> Void)?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		if #available(iOS 13.0, *) {
+			isModalInPresentation = true
+		}
+
+		picker.sourceType = .camera
+		picker.allowsEditing = false
+		picker.showsCameraControls = false
+		picker.cameraOverlayView = overlayView
+		picker.delegate = self
+
+		addChild(picker)
+		view.addSubview(picker.view)
+		picker.view.frame = view.bounds
+		overlayView.frame = view.bounds
+		picker.didMove(toParent: self)
+
+		// center picker in screen
+		let screenSize = picker.view.bounds.size
+		let cameraAspectRatio: CGFloat = 4.0 / 3.0 // Typical for iOS camera
+		let previewHeight = screenSize.width * cameraAspectRatio
+		let verticalOffset = (screenSize.height - previewHeight) / 2
+		picker.cameraViewTransform = CGAffineTransform(translationX: 0, y: verticalOffset)
+
+		// Set up overlay transitions
+		overlayView.shutterButton.addTarget(self, action: #selector(shutterTapped), for: .touchUpInside)
+		overlayView.retakeButton.addTarget(self, action: #selector(retakeTapped), for: .touchUpInside)
+		overlayView.acceptButton.addTarget(self, action: #selector(acceptTapped), for: .touchUpInside)
+		overlayView.cancelButton.addTarget(self, action: #selector(cancelTapped), for: .touchUpInside)
+	}
+
+	@objc private func shutterTapped() {
+		picker.takePicture()
+		overlayView.acceptButton.isEnabled = false
+		overlayView.switchToState(.reviewing)
+	}
+
+	@objc private func retakeTapped() {
+		overlayView.hidePreview()
+		overlayView.switchToState(.capturing)
+	}
+
+	@objc private func acceptTapped() {
+		if let image = capturedImage,
+		   let data = capturedImageData
+		{
+			dismiss(animated: true)
+			onAccept?(image, data)
+		} else {
+			onError?()
+			dismiss(animated: true)
+		}
+	}
+
+	@objc private func cancelTapped() {
+		dismiss(animated: true)
+		onCancel?()
+	}
+
+	func imagePickerController(_ picker: UIImagePickerController,
+	                           didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any])
+	{
+		// get image
+		let location = locationManager?.location
+		let heading = locationManager?.heading
+		guard let image = info[.originalImage] as? UIImage else {
+			onError?()
+			dismiss(animated: true)
+			return
+		}
+		overlayView.showPreview(image)
+		overlayView.switchToState(.reviewing)
+
+		// converting to data is slow so we do it in the background so we can show the preview immediately
+		DispatchQueue.global(qos: .userInitiated).async {
+			let data = dataFor(image: image, location: location, heading: heading)
+			DispatchQueue.main.sync {
+				guard let data else {
+					self.onError?()
+					self.dismiss(animated: true)
+					return
+				}
+				self.capturedImage = image
+				self.capturedImageData = data
+				self.overlayView.acceptButton.isEnabled = true
+			}
+		}
+	}
+
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		dismiss(animated: true)
+	}
+}

commit 585415685301a3e9aadca731bce9acdd3fcd3b7d

Fix bug introduced in 7e72aa28c132259427116afc58e7f782b4f04213 that stopped location ball heading from updating

src/Shared/MapView.swift
@@ -1651,8 +1651,6 @@ final class MapView: UIView, MapViewProgress, CLLocationManagerDelegate, UIActio
 
 	private var locationManagerSmoothHeading = 0.0
 	func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-		guard gpsState == .HEADING else { return }
-
 		let accuracy = newHeading.headingAccuracy
 		let heading = self.heading(for: newHeading)
 
@@ -1735,7 +1733,6 @@ final class MapView: UIView, MapViewProgress, CLLocationManagerDelegate, UIActio
 	}
 
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-		guard gpsState != .NONE else { return }
 		for location in locations {
 			locationUpdated(to: location)
 		}

src/iOS/Panoramax.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2025 Bryce Cogswell. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 @preconcurrency import WebKit
 
@@ -234,6 +235,7 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 	var photoID = ""
 	var delegate: PanororamaxDelegate?
 	var location: LatLon = .zero
+	let locationManager = CLLocationManager()
 
 	@IBOutlet var photoView: UIImageView!
 	@IBOutlet var photoUser: UILabel!
@@ -283,25 +285,12 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 		}
 
 		// start location services in case user takes a photo
-		AppDelegate.shared.mapView.locationManager.startUpdatingLocation()
-		AppDelegate.shared.mapView.locationManager.startUpdatingHeading()
+		locationManager.startUpdatingLocation()
+		locationManager.startUpdatingHeading()
 	}
 
 	override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
-
-		// return location services to expected state
-		if isBeingDismissed || navigationController?.isBeingDismissed == true {
-			switch AppDelegate.shared.mapView.gpsState {
-			case .NONE:
-				AppDelegate.shared.mapView.locationManager.stopUpdatingLocation()
-				AppDelegate.shared.mapView.locationManager.stopUpdatingHeading()
-			case .LOCATION:
-				AppDelegate.shared.mapView.locationManager.stopUpdatingHeading()
-			case .HEADING:
-				break
-			}
-		}
 	}
 
 	@IBAction
@@ -330,7 +319,7 @@ class PanoramaxViewController: UIViewController, UIImagePickerControllerDelegate
 			style: .default,
 			handler: { [self] _ in
 				photoPicker = PhotoCapture()
-				photoPicker.locationManager = AppDelegate.shared.mapView.locationManager
+				photoPicker.locationManager = locationManager
 				photoPicker.onCancel = {}
 				photoPicker.onError = {}
 				photoPicker.onAccept = { image, imageData in