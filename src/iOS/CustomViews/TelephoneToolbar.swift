dc2853e87037df8fa50ad13f99a527b907dc27b9
Bryce Cogswell <bryceco@yahoo.com>
2025-11-10 18:39:19 -0800

    Provide an explicit background TelephoneToolbar

--- src/iOS/CustomViews/TelephoneToolbar.swift
+++ src/iOS/CustomViews/TelephoneToolbar.swift
@@ -8,35 +8,60 @@
 
 import UIKit
 
-class TelephoneToolbar: UIToolbar {
+class TelephoneToolbar: UIControl {
 	weak var textField: UITextField!
 	let codes = AppDelegate.shared.mapView.currentRegion.callingCodes
 
 	init(forTextField textfield: UITextField, frame: CGRect) {
 		textField = textfield
 		super.init(frame: CGRect(x: 0, y: 0, width: textField.frame.size.width, height: 44))
+
+		let blurEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+		let blurView = UIVisualEffectView(effect: blurEffect)
+		addSubview(blurView)
+
 		let space = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
 		space.width = 10.0
-		items = [
+		let toolbar = UIToolbar(frame: .zero)
+		toolbar.items = [
 			Self.makeButton(
 				title: "+\(codes.first ?? "1")",
-				target: self,
-				action: #selector(setCallingCodeText(_:))),
+				action: { _ in
+					if let text = self.textField.text,
+					   !text.hasPrefix("+")
+					{
+						let code = self.codes.first ?? "1"
+						self.textField.text = "+" + code + " " + text
+					}
+				}),
 			space,
 			Self.makeButton(
 				title: NSLocalizedString("Space", comment: "Space key on the keyboard"),
-				target: self,
-				action: #selector(insertSpace(_:))),
+				action: { _ in
+					self.textField.insertText(" ")
+				}),
 			space,
 			Self.makeButton(
 				title: "\u{2014}",
-				target: self,
-				action: #selector(insertDash(_:))),
+				action: { _ in
+					self.textField.insertText("-")
+				}),
 			UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
 			UIBarButtonItem(barButtonSystemItem: .done,
 			                target: self,
 			                action: #selector(done(_:)))
 		]
+		blurView.contentView.addSubview(toolbar)
+
+		for view in [blurView, toolbar] {
+			view.translatesAutoresizingMaskIntoConstraints = false
+			NSLayoutConstraint.activate([
+				view.topAnchor.constraint(equalTo: self.topAnchor),
+				view.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+				view.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+				view.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+			])
+		}
 	}
 
 	@available(*, unavailable)
@@ -48,35 +73,17 @@ class TelephoneToolbar: UIToolbar {
 		textField.resignFirstResponder()
 	}
 
-	@objc func setCallingCodeText(_ sender: Any?) {
-		if let text = textField.text,
-		   !text.hasPrefix("+")
-		{
-			let code = codes.first ?? "1"
-			textField.text = "+" + code + " " + text
-		}
-	}
-
-	@objc func insertSpace(_ sender: Any?) {
-		textField.insertText(" ")
-	}
-
-	@objc func insertDash(_ sender: Any?) {
-		textField.insertText("-")
-	}
-
 	private class func makeButton(title: String,
-	                              target: AnyObject,
-	                              action: Selector) -> UIBarButtonItem
+	                              action: @escaping (UIButton) -> Void) -> UIBarButtonItem
 	{
-		let button = UIButton(type: .custom)
+		let button = ButtonClosure(type: .custom)
 		button.setTitle(title, for: .normal)
 		button.setTitleColor(UIColor.systemBlue, for: .normal)
 		button.contentEdgeInsets = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
 		button.layer.borderWidth = 1
 		button.layer.cornerRadius = 10
 		button.layer.borderColor = UIColor.systemBlue.cgColor
-		button.addTarget(target, action: action, for: .touchUpInside)
+		button.onTap = action
 		return UIBarButtonItem(customView: button)
 	}
 }
