2f0fefa9f257a6facae5d203a7051d6ad30a071e
Bryce Cogswell <bryceco@yahoo.com>
2025-12-15 09:53:34 -0800

    Remove MapLibre localization code that is prone to crashing

--- src/Shared/Tiles/MapLibreVectorTilesView.swift
+++ src/Shared/Tiles/MapLibreVectorTilesView.swift
@@ -58,28 +58,6 @@ class MapLibreVectorTilesView: MLNMapView, MLNMapViewDelegate {
 	func mapView(_ map: MLNMapView, didFinishLoading style: MLNStyle) {
 		let locale = Locale(identifier: PresetLanguages.preferredLanguageCode())
 		style.localizeLabels(into: locale)
-
-		for source: MLNSource in style.sources {
-			if let tileSource = source as? MLNTileSource {
-				for attrib in tileSource.attributionInfos {
-					print("\(attrib)")
-				}
-			}
-		}
-
-		let bad = [
-			"place_town",
-			"place_city"
-		]
-		for layer in style.layers {
-			if let layer = layer as? MLNSymbolStyleLayer {
-				// an icon and/or label
-				if bad.contains(layer.identifier) {
-					continue
-				}
-				layer.text = layer.text.mgl_expressionLocalized(into: locale)
-			}
-		}
 	}
 
 	func mapViewDidFinishLoadingMap(_ mapView: MLNMapView) {}
