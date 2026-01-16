547be019c25fdc709a66213367f4afaba3da32d6
Bryce Cogswell <bryceco@yahoo.com>
2026-01-16 10:50:49 -0800

    Add a property wrapper for NotificationService

---
+++ src/Shared/AppState.swift
@@ -0,0 +1,9 @@
+//
+//  AppState.swift
+//  Go Map!!
+//
+//  Created by Bryce Cogswell on 1/16/26.
+//  Copyright © 2026 Bryce Cogswell. All rights reserved.
+//
+
+import Foundation
--- src/Shared/Utility/NotificationService.swift
+++ src/Shared/Utility/NotificationService.swift
@@ -19,13 +19,14 @@ final class NotificationService<T> {
 
 	func subscribe(_ observer: AnyObject, handler: @escaping (T) -> Void) {
 		queue.sync {
+			subscribers = subscribers.filter { $0.object !== observer && $0.object != nil }
 			subscribers.append(Subscriber(object: observer, callback: handler))
 		}
 	}
 
 	func unsubscribe(_ observer: AnyObject) {
 		queue.sync {
-			subscribers = subscribers.filter { $0.object !== observer }
+			subscribers = subscribers.filter { $0.object !== observer && $0.object != nil }
 		}
 	}
 
@@ -34,7 +35,10 @@ final class NotificationService<T> {
 			// Synchronous path for main thread
 			var currentSubscribers: [Subscriber] = []
 			queue.sync {
-				self.subscribers = self.subscribers.filter { $0.object != nil }
+				if self.subscribers.contains(where: { $0.object == nil }) {
+					// This is rare, so only do if necessary
+					self.subscribers = self.subscribers.filter { $0.object != nil }
+				}
 				currentSubscribers = self.subscribers
 			}
 			for subscriber in currentSubscribers {
@@ -57,5 +61,45 @@ final class NotificationService<T> {
 extension NotificationService where T == Void {
 	func notify() {
 		notify(())
+	}
+}
+
+@propertyWrapper
+final class Notify<T> {
+	private let service = NotificationService<T>()
+	private var value: T
+
+	var wrappedValue: T {
+		get { value }
+		set {
+			value = newValue
+			service.notify(newValue)
+		}
+	}
+
+	var projectedValue: Notify<T> {
+		return self
+	}
+
+	func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, T>,
+	                             on object: Root)
+	{
+		object[keyPath: keyPath] = value
+
+		service.subscribe(object) { [weak object] newValue in
+			object?[keyPath: keyPath] = newValue
+		}
+	}
+
+	init(wrappedValue: T) {
+		self.value = wrappedValue
+	}
+
+	func subscribe(_ observer: AnyObject, handler: @escaping (T) -> Void) {
+		service.subscribe(observer, handler: handler)
+	}
+
+	func unsubscribe(_ observer: AnyObject) {
+		service.unsubscribe(observer)
 	}
 }
