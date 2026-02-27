135d22449d055cfa7e4800b75dc1a1dd3d0c121b
Bryce Cogswell <bryceco@yahoo.com>
2026-02-26 17:47:05 -0800

    Rewrite DraggableButton to use a simpler and more robust approach.

--- src/iOS/CustomViews/DraggableButton.swift
+++ src/iOS/CustomViews/DraggableButton.swift
@@ -16,43 +16,21 @@ import UIKit
 // target selector if it is close by. There are other ways to accomplish this, such
 // as having the drag gesture recognizer cancel the tap recognizer.
 class DraggableButton: UIButton {
-	private var tapPos = CGPoint.zero
-	private var target: NSObject?
-	private var requestedAction: Selector?
+	private var touchDownPos = CGPoint.zero
 
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-		addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
+	override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+		touchDownPos = touch.location(in: nil)
+		return super.beginTracking(touch, with: event)
 	}
 
-	required init?(coder: NSCoder) {
-		super.init(coder: coder)
-		addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
-	}
-
-	@objc func touchDown(_ sender: Any?) {
-		tapPos = frame.origin
-	}
-
-	override func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControl.Event) {
-		if controlEvents == .touchUpInside {
-			// we need to intercept the event and ignore it potentially
-			self.target = (target as? NSObject)!
-			requestedAction = action
-			super.addTarget(self, action: #selector(touchUpInside), for: controlEvents)
-		} else {
-			super.addTarget(target, action: action, for: controlEvents)
+	override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+		if let touch {
+			let endPos = touch.location(in: nil)
+			let distance = hypot(touchDownPos.x - endPos.x, touchDownPos.y - endPos.y)
+			if distance < 10 {
+				sendActions(for: .touchUpInside)
+			}
 		}
-	}
-
-	@objc func touchUpInside() {
-		let pos1 = tapPos
-		let pos2 = frame.origin
-		if hypot(pos1.x - pos2.x, pos1.y - pos2.y) > frame.size.width / 2 {
-			// we moved, so ignore event
-		} else {
-			// good to go: invoke the original action
-			_ = target!.perform(requestedAction!, with: self).takeUnretainedValue()
-		}
+		super.endTracking(touch, with: event)
 	}
 }
