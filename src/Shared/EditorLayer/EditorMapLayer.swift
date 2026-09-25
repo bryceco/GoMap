fd4a316408abfb2b9bf1b16d2e8feb8acb1fb199
Bryce Cogswell <bryceco@yahoo.com>
2026-09-25 13:51:24 +0200

    Fix runtime warning

--- src/Shared/EditorLayer/EditorMapLayer.swift
+++ src/Shared/EditorLayer/EditorMapLayer.swift
@@ -508,12 +508,14 @@ final class EditorMapLayer: CALayer {
 		mapData.downloadMissingData(inRect: box, withProgress: progress, didUpdate: { [weak self] error in
 			guard let self else { return }
 			if let error {
-				// if we've been hidden don't bother displaying errors
-				if !isHidden, !owner.isHidden {
-					// present error asynchrounously so we don't interrupt the current UI action
-					DispatchQueue.main.async(execute: { [weak self] in
-						self?.display.presentError(title: nil, error: error, flash: true)
-					})
+				// present error asynchronously so we don't interrupt the current UI action
+				DispatchQueue.main.async { [weak self] in
+					// if we've been hidden don't bother displaying errors
+					guard let self,
+					      !self.isHidden,
+					      !self.owner.isHidden
+					else { return }
+					self.display.presentError(title: nil, error: error, flash: true)
 				}
 				return
 			}
